4c8931a458507e721472306d7c999b052cd5b433
eddturner <eddturner@gmai.com>
2019-04-11 20:12:13 +0100

    reformat

--- indexer/src/main/java/uk/ac/ebi/uniprot/indexer/common/listeners/ListenerConfig.java
+++ indexer/src/main/java/uk/ac/ebi/uniprot/indexer/common/listeners/ListenerConfig.java
@@ -5,26 +5,24 @@ import org.springframework.batch.core.JobExecutionListener;
 import org.springframework.batch.core.StepExecutionListener;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
 /**
- *
  * @author lgonzales
  */
 @Configuration
 public class ListenerConfig {
-
     @Bean
     public JobExecutionListener jobListener() {
         return new LogJobListener();
-
     }
 
     @Bean
-    public StepExecutionListener stepListener(){
+    public StepExecutionListener stepListener() {
         return new LogStepListener();
     }
 
     @Bean
-    public ChunkListener chunkListener(){
+    public ChunkListener chunkListener() {
         return new LogChunkListener();
     }
 }
