8c06c383076d17aeb19aa2d7f0a5fe10dd7b33fe
eddturner <eddturner@gmai.com>
2019-04-18 13:35:58 +0100

    use equalsandhashcode(callsuper=true) lombok annotation

--- indexer/src/main/java/uk/ac/ebi/uniprot/indexer/uniprotkb/model/InactiveEntryDocumentPair.java
+++ indexer/src/main/java/uk/ac/ebi/uniprot/indexer/uniprotkb/model/InactiveEntryDocumentPair.java
@@ -5,7 +5,12 @@ import uk.ac.ebi.uniprot.indexer.common.model.AbstractEntryDocumentPair;
 import uk.ac.ebi.uniprot.indexer.uniprot.inactiveentry.InactiveUniProtEntry;
 import uk.ac.ebi.uniprot.search.document.uniprot.UniProtDocument;
 
-@EqualsAndHashCode
+/**
+ * Created 18/04/19
+ *
+ * @author Edd
+ */
+@EqualsAndHashCode(callSuper = true)
 public class InactiveEntryDocumentPair extends AbstractEntryDocumentPair<InactiveUniProtEntry, UniProtDocument> {
     public InactiveEntryDocumentPair(InactiveUniProtEntry entry) {
         super(entry);
--- indexer/src/main/java/uk/ac/ebi/uniprot/indexer/uniprotkb/model/UniProtEntryDocumentPair.java
+++ indexer/src/main/java/uk/ac/ebi/uniprot/indexer/uniprotkb/model/UniProtEntryDocumentPair.java
@@ -6,17 +6,11 @@ import uk.ac.ebi.uniprot.indexer.common.model.AbstractEntryDocumentPair;
 import uk.ac.ebi.uniprot.search.document.uniprot.UniProtDocument;
 
 /**
- * Represents a {@link UniProtEntry} and an associated {@link UniProtDocument}. The purpose of this class
- * is to record the pair of entities through a Spring Batch process and keep them together, when Spring Batch
- * natural separates the two in different phases (entries are converted to documents in different parts of the Step).
- * This means we can now, for example, when writing documents, if there is a write error, then we can also
- * write the corresponding {@link UniProtEntry} to a log file for future reference / reprocessing.
- * <p>
  * Created 12/04/19
  *
  * @author Edd
  */
-@EqualsAndHashCode
+@EqualsAndHashCode(callSuper = true)
 public class UniProtEntryDocumentPair extends AbstractEntryDocumentPair<UniProtEntry, UniProtDocument> {
     public UniProtEntryDocumentPair(UniProtEntry entry) {
         super(entry);
