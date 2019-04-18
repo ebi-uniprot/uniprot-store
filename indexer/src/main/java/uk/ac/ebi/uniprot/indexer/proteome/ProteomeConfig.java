b66ea76dd458bc3f73194f0e9eaeb6dc5ac13a14
jluo <jluo@ebi.ac.uk>
2019-04-18 15:27:54 +0100

    add more classes

--- indexer/src/main/java/uk/ac/ebi/uniprot/indexer/proteome/ProteomeConfig.java
+++ indexer/src/main/java/uk/ac/ebi/uniprot/indexer/proteome/ProteomeConfig.java
@@ -1,10 +1,18 @@
 package uk.ac.ebi.uniprot.indexer.proteome;
 
+import java.io.File;
+import java.io.IOException;
+
+import org.springframework.batch.item.ItemReader;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import lombok.Data;
+import uk.ac.ebi.uniprot.indexer.uniprot.taxonomy.FileNodeIterable;
+import uk.ac.ebi.uniprot.indexer.uniprot.taxonomy.TaxonomyMapRepo;
+import uk.ac.ebi.uniprot.indexer.uniprot.taxonomy.TaxonomyRepo;
+import uk.ac.ebi.uniprot.xml.jaxb.proteome.Proteome;
 
 /**
  *
@@ -15,12 +23,24 @@ import lombok.Data;
 @Configuration
 @Data
 public class ProteomeConfig {
-	  @Value(("${proteome.xml.file}"))
-	  private String filename;
+	  @Value(("${proteome.indexing.xml.file}"))
+	  private String proteomeXmlFilename;
+	  
+	  @Value(("${uniprotkb.indexing.taxonomyFile}"))
+	  private String taxonomyFile;
 	  
 	  @Bean("ProteomeEntryProcessor")
 	  public ProteomeEntryProcessor proteomeEntryProcessor() {
-		  return 
+		  return new ProteomeEntryProcessor(createTaxonomyRepo());
 	  }
+	  
+	    @Bean(name = "proteomeXmlReader")
+	    public ItemReader<Proteome> xrefReader() throws IOException {
+	        return new ProteomeXmlEntryReader(proteomeXmlFilename);
+	    }
+	  
+	    private TaxonomyRepo createTaxonomyRepo() {
+	        return new TaxonomyMapRepo(new FileNodeIterable(new File(taxonomyFile)));
+	    }
 }
 
