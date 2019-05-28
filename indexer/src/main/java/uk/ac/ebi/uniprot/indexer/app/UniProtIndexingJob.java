3784192d2145b6efe2ac7dffd06f0606b6180e55
eddturner <eddturner@gmai.com>
2019-05-28 21:03:47 +0100

    testing ways of limiting beans created at spring boot app startup, using custom componentscans and conditionalon annotations.

---
+++ indexer/src/main/java/uk/ac/ebi/uniprot/indexer/app/UniProtIndexingJob.java
@@ -0,0 +1,21 @@
+package uk.ac.ebi.uniprot.indexer.app;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * Created 28/05/19
+ *
+ * @author Edd
+ */
+@Target({ElementType.TYPE})
+@Retention(RetentionPolicy.RUNTIME)
+@ComponentScan
+@Configuration
+public @interface UniProtIndexingJob {
+}
