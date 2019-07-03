7b93613f754efdf9f5a6fc570ae88879048e0fcb
eddturner <eddturner@gmai.com>
2019-07-03 21:46:58 +0100

    add todo

--- indexer/src/main/java/uk/ac/ebi/uniprot/indexer/uniprotkb/processor/UniProtEntryDocumentPairProcessor.java
+++ indexer/src/main/java/uk/ac/ebi/uniprot/indexer/uniprotkb/processor/UniProtEntryDocumentPairProcessor.java
@@ -26,6 +26,7 @@ public class UniProtEntryDocumentPairProcessor extends EntryDocumentPairProcesso
     }
 
     @BeforeStep
+    // TODO: 03/07/19 no longer triggered with async
     public void setStepExecution(final StepExecution stepExecution) {
         ExecutionContext executionContext = stepExecution.getJobExecution().getExecutionContext();
         executionContext.put(Constants.SUGGESTIONS_MAP, converter.getSuggestions());
