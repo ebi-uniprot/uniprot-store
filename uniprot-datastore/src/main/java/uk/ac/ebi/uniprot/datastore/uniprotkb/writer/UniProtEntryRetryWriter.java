8c19848cf2058a2553c1ae4bd9866fdd8de9c5e5
eddturner <eddturner@gmai.com>
2019-07-27 13:07:03 +0100

    the uniprotentry writer

--- uniprot-datastore/src/main/java/uk/ac/ebi/uniprot/datastore/uniprotkb/writer/UniProtEntryRetryWriter.java
+++ uniprot-datastore/src/main/java/uk/ac/ebi/uniprot/datastore/uniprotkb/writer/UniProtEntryRetryWriter.java
@@ -1,18 +1,24 @@
 package uk.ac.ebi.uniprot.datastore.uniprotkb.writer;
 
 import net.jodah.failsafe.RetryPolicy;
+import org.springframework.scheduling.annotation.Async;
 import uk.ac.ebi.uniprot.datastore.Store;
-import uk.ac.ebi.uniprot.datastore.writer.ItemRetryWriter;
+import uk.ac.ebi.uniprot.datastore.common.writer.ItemRetryWriter;
 import uk.ac.ebi.uniprot.domain.uniprot.UniProtEntry;
+import uk.ac.ebi.uniprot.domain.uniprot.builder.UniProtEntryBuilder;
 import uk.ac.ebi.uniprot.flatfile.parser.ffwriter.impl.UniProtFlatfileWriter;
+import uk.ebi.uniprot.scorer.uniprotkb.UniProtEntryScored;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * Created 25/07/19
  *
  * @author Edd
  */
-public class UniProtEntryRetryWriter extends ItemRetryWriter<UniProtEntry> {
-    public UniProtEntryRetryWriter(Store store, RetryPolicy<Object> retryPolicy) {
+public class UniProtEntryRetryWriter extends ItemRetryWriter<UniProtEntry, UniProtEntry> {
+    public UniProtEntryRetryWriter(Store<UniProtEntry> store, RetryPolicy<Object> retryPolicy) {
         super(store, retryPolicy);
     }
 
@@ -27,7 +33,22 @@ public class UniProtEntryRetryWriter extends ItemRetryWriter<UniProtEntry> {
     }
 
     @Override
-    public Object itemToEntry(UniProtEntry item) {
+    public UniProtEntry itemToEntry(UniProtEntry item) {
         return item;
+    }
+
+    @Override
+    @Async(ITEM_WRITER_TASK_EXECUTOR)
+    public void write(List<? extends UniProtEntry> items) {
+        super.write(items.stream().map(this::addAnnotationScore).collect(Collectors.toList()));
+    }
+
+    // TODO: 26/07/19 why don't we set the annotation score by default when we read the entry? 
+    private UniProtEntry addAnnotationScore(UniProtEntry entry) {
+        UniProtEntryScored entryScored = new UniProtEntryScored(entry);
+        double score = entryScored.score();
+        UniProtEntryBuilder.ActiveEntryBuilder builder = new UniProtEntryBuilder().from(entry);
+        builder.annotationScore(score);
+        return  builder.build();
     }
 }
