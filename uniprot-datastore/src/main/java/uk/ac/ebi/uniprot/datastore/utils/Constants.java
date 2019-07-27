746e41ccc4cdfca18c519c109ba5be1461acba80
eddturner <eddturner@gmai.com>
2019-07-27 13:10:21 +0100

    constants

--- uniprot-datastore/src/main/java/uk/ac/ebi/uniprot/datastore/utils/Constants.java
+++ uniprot-datastore/src/main/java/uk/ac/ebi/uniprot/datastore/utils/Constants.java
@@ -2,10 +2,12 @@ package uk.ac.ebi.uniprot.datastore.utils;
 
 
 public class Constants {
-    public static final String UNIPROTKB_DATASTORE_JOB = "UniProtKB_DataStore_Job";
-    public static final String UNIPROTKB_DATASTORE_STEP = "UniProtKB_DataStore_Step";
-    public static final String DATASTORE_FAILED_ENTRIES_COUNT_KEY = "DATASTORE_FAILED_ENTRIES_COUNT_KEY";
-    public static final String DATASTORE_WRITTEN_ENTRIES_COUNT_KEY = "DATASTORE_WRITTEN_ENTRIES_COUNT_KEY";
+    private Constants(){}
+
+    public static final String UNIPROTKB_STORE_JOB = "UniProtKB_Store_Job";
+    public static final String UNIPROTKB_STORE_STEP = "UniProtKB_Store_Step";
+    public static final String STORE_FAILED_ENTRIES_COUNT_KEY = "STORE_FAILED_ENTRIES_COUNT_KEY";
+    public static final String STORE_WRITTEN_ENTRIES_COUNT_KEY = "STORE_WRITTEN_ENTRIES_COUNT_KEY";
 
     public static final String ENTRIES_TO_WRITE_COUNTER = "Entries_to_write_counter";
 }
