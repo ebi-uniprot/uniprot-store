3069f730e500aaae0e5f264e34c44a9bae43b3c0
eddturner <eddturner@gmai.com>
2019-07-28 21:40:48 +0100

    stub

---
+++ uniprot-datastore/src/test/java/uk/ac/ebi/uniprot/datastore/uniprotkb/UniProtKBJobIT.java
@@ -0,0 +1,12 @@
+package uk.ac.ebi.uniprot.datastore.uniprotkb;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * Created 28/07/19
+ *
+ * @author Edd
+ */
+class UniProtKBJobIT {
+
+}
