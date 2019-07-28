74999ec72f28a86ef21c6d5345ee215480fa1c8f
eddturner <eddturner@gmai.com>
2019-07-28 21:29:31 +0100

    reusable taskexecutorproperties converter

---
+++ common-job/src/main/java/uk/ac/ebi/uniprot/job/common/concurrent/TaskExecutorPropertiesConverter.java
@@ -0,0 +1,27 @@
+package uk.ac.ebi.uniprot.job.common.concurrent;
+
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+import uk.ac.ebi.uniprot.common.concurrency.TaskExecutorProperties;
+
+import java.util.concurrent.ThreadPoolExecutor;
+
+/**
+ * Created 28/07/19
+ *
+ * @author Edd
+ */
+public class TaskExecutorPropertiesConverter {
+    public static ThreadPoolTaskExecutor createThreadPoolTaskExecutor(TaskExecutorProperties properties) {
+        ThreadPoolTaskExecutor taskExecutor = new ThreadPoolTaskExecutor();
+        taskExecutor.setCorePoolSize(properties.getCorePoolSize());
+        taskExecutor.setMaxPoolSize(properties.getMaxPoolSize());
+        taskExecutor.setQueueCapacity(properties.getQueueCapacity());
+        taskExecutor.setKeepAliveSeconds(properties.getKeepAliveSeconds());
+        taskExecutor.setAllowCoreThreadTimeOut(properties.isAllowCoreThreadTimeout());
+        taskExecutor.setWaitForTasksToCompleteOnShutdown(properties.isWaitForTasksToCompleteOnShutdown());
+        taskExecutor.setRejectedExecutionHandler(new ThreadPoolExecutor.CallerRunsPolicy());
+        taskExecutor.initialize();
+        taskExecutor.setThreadNamePrefix(properties.getThreadNamePrefix());
+        return taskExecutor;
+    }
+}
