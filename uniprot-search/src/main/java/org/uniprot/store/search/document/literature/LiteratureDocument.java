5bec30c75c0945d19226c5224cc04ca253ff9ea9
Shadab Ahmad <sahmad@ebi.ac.uk>
2019-11-11 16:37:55 +0000

    TRM-23135 provide default and all args constructor

--- uniprot-search/src/main/java/org/uniprot/store/search/document/literature/LiteratureDocument.java
+++ uniprot-search/src/main/java/org/uniprot/store/search/document/literature/LiteratureDocument.java
@@ -1,7 +1,9 @@
 package org.uniprot.store.search.document.literature;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import org.apache.solr.client.solrj.beans.Field;
 import org.uniprot.store.search.document.Document;
 
@@ -10,6 +12,8 @@ import java.util.Set;
 
 @Getter
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class LiteratureDocument implements Document {
 
     @Field
