7c9896907591afccf0d8b6239398bd0fe4f51e6c
Edward Turner <eddturner@ebi.ac.uk>
2019-11-12 16:44:50 +0000

    initial commit for impl

---
+++ uniprot-search/src/main/java/org/uniprot/store/search/domain2/SearchFieldFactory.java
@@ -0,0 +1,13 @@
+package org.uniprot.store.search.domain2;
+
+/**
+ * Created 12/11/2019
+ *
+ * @author Edd
+ */
+public class SearchFieldFactory {
+    // getSearchItems
+    // getSearchFields
+    // getTermFields
+    // getRangeFields
+}
---
+++ uniprot-search/src/main/java/org/uniprot/store/search/domain2/TermField.java
@@ -0,0 +1,9 @@
+package org.uniprot.store.search.domain2;
+
+/**
+ * Created 12/11/2019
+ *
+ * @author Edd
+ */
+public class TermField {
+}
