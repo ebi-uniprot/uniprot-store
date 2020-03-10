2f7b40fac449ac66487dd1db3a235f482ad6ddd8
Edward Turner <eddturner@ebi.ac.uk>
2020-03-10 12:27:15 +0000

    auto generated all dr_ paths and filters ... would have taken too long otherwise

---
+++ uniprot-config/src/main/java/org/uniprot/store/config/uniprotkb/AddCrossRefsForReturnFields.java
@@ -0,0 +1,55 @@
+package org.uniprot.store.config.uniprotkb;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.atomic.AtomicInteger;
+
+/**
+ * Created 03/03/2020
+ *
+ * @author Edd
+ */
+public class AddCrossRefsForReturnFields {
+    private static List<JsonNode> fields;
+    private static ObjectMapper om;
+
+    public static void main(String[] args) throws IOException {
+        om = new ObjectMapper();
+        fields = new ArrayList<>();
+        JsonNode rootNode =
+                om.readTree(
+                        new File(
+                                "/home/eddturner/working/intellij/website/uniprot-store/uniprot-config/src/main/resources/result-fields-config/uniprotkb-result-fields.json"));
+
+        AtomicInteger itemPosition = new AtomicInteger();
+        rootNode.iterator().forEachRemaining(node -> convertDRField(node, itemPosition));
+
+        printFields();
+    }
+
+    private static void convertDRField(JsonNode node, AtomicInteger itemPosition) {
+        if (node.has("name")) {
+            String name = node.get("name").asText();
+            if (name.startsWith("dr_")) {
+                ((ObjectNode) node).put("path", "databaseCrossReferences");
+                ((ObjectNode) node).put("filter", "[?(@.databaseType=='" + node.get("label").asText() + "')]");
+            }
+        }
+        fields.add(node);
+    }
+
+    private static void printFields() {
+        try {
+            System.out.println(om.writerWithDefaultPrettyPrinter().writeValueAsString(fields));
+        } catch (JsonProcessingException e) {
+            throw new IllegalStateException(e);
+        }
+    }
+}
