f4db7c427bdfefd3950614713ccd21ed5772e5c0
Edward Turner <eddturner@ebi.ac.uk>
2020-03-11 16:29:54 +0000

    remove dead code

--- uniprot-config/src/test/java/org/uniprot/store/config/schema/AbstractFieldValidatorTest.java
+++ uniprot-config/src/test/java/org/uniprot/store/config/schema/AbstractFieldValidatorTest.java
@@ -5,13 +5,8 @@ import org.apache.commons.lang3.StringUtils;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.uniprot.store.config.model.Field;
-import org.uniprot.store.config.searchfield.model.SearchFieldDataType;
 import org.uniprot.store.config.searchfield.model.SearchFieldItemType;
-import org.uniprot.store.config.searchfield.model.SearchFieldType;
 
-import javax.validation.constraints.NotNull;
-import javax.validation.constraints.PositiveOrZero;
-import java.io.Serializable;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Set;
@@ -158,24 +153,12 @@ class AbstractFieldValidatorTest {
     }
 
     @Data
-    private static class FakeFieldItem implements Field, Serializable {
-        private static final long serialVersionUID = -1835246966684124878L;
-        @NotNull private String id;
+    private static class FakeFieldItem implements Field {
+        private String id;
         private String parentId;
-        @PositiveOrZero private Integer childNumber;
-        @NotNull @PositiveOrZero private Integer seqNumber;
-        @NotNull private SearchFieldItemType itemType;
-        private SearchFieldType fieldType;
+        private Integer childNumber;
+        private Integer seqNumber;
+        private SearchFieldItemType itemType;
         private String label;
-        private String fieldName;
-        private SearchFieldDataType dataType;
-        private String description;
-        private String example;
-        private String validRegex;
-        private List<org.uniprot.store.config.searchfield.model.SearchFieldItem.Value> values;
-        private String autoComplete;
-        private String autoCompleteQueryField;
-        private String autoCompleteQueryFieldValidRegex;
-        private String sortFieldId;
     }
 }
