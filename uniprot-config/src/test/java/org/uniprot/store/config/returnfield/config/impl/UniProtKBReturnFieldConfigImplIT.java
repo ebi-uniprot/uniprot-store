cf184ae8d70e72071828b9fc8a25774575c1887b
eddturner <eddturner@gmai.com>
2020-03-17 08:54:31 +0000

    test to check that all sort fields defined in return fields def for uniprotkb are valid.

---
+++ uniprot-config/src/test/java/org/uniprot/store/config/returnfield/config/impl/UniProtKBReturnFieldConfigImplIT.java
@@ -0,0 +1,49 @@
+package org.uniprot.store.config.returnfield.config.impl;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.uniprot.core.util.Utils;
+import org.uniprot.store.config.UniProtDataType;
+import org.uniprot.store.config.returnfield.config.ReturnFieldConfig;
+import org.uniprot.store.config.returnfield.factory.ReturnFieldConfigFactory;
+import org.uniprot.store.config.returnfield.model.ReturnField;
+import org.uniprot.store.config.searchfield.common.SearchFieldConfig;
+import org.uniprot.store.config.searchfield.factory.SearchFieldConfigFactory;
+
+import java.util.stream.Stream;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+/**
+ * Created 17/03/20
+ *
+ * @author Edd
+ */
+class UniProtKBReturnFieldConfigImplIT {
+    private static ReturnFieldConfig returnFieldConfig;
+    private static SearchFieldConfig searchFieldConfig;
+
+    @BeforeAll
+    static void setUp() {
+        returnFieldConfig =
+                ReturnFieldConfigFactory.getReturnFieldConfig(UniProtDataType.UNIPROTKB);
+        searchFieldConfig =
+                SearchFieldConfigFactory.getSearchFieldConfig(UniProtDataType.UNIPROTKB);
+    }
+
+    @ParameterizedTest(name = "Sort field [{0}] for return field exists?")
+    @MethodSource("provideSortFields")
+    void validReturnFieldSortFieldDefined(String returnFieldsSortField) {
+        assertThat(searchFieldConfig.correspondingSortFieldExists(returnFieldsSortField), is(true));
+    }
+
+    private static Stream<Arguments> provideSortFields() {
+        return returnFieldConfig.getReturnFields().stream()
+                .filter(field -> Utils.notNullNotEmpty(field.getSortField()))
+                .map(ReturnField::getSortField)
+                .map(Arguments::of);
+    }
+}
