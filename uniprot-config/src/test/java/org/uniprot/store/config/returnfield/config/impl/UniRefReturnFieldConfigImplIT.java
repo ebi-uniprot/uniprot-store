18d7aff9459b15bd9f95dcfeb3631cec8e687474
lgonzales <lgonzales@ebi.ac.uk>
2020-03-26 12:12:39 +0000

    - add uniRef field config integration test for sort and tsv

--- uniprot-config/src/test/java/org/uniprot/store/config/returnfield/config/impl/UniRefReturnFieldConfigImplIT.java
+++ uniprot-config/src/test/java/org/uniprot/store/config/returnfield/config/impl/UniRefReturnFieldConfigImplIT.java
@@ -1,21 +1,12 @@
 package org.uniprot.store.config.returnfield.config.impl;
 
-import static org.hamcrest.CoreMatchers.is;
-import static org.hamcrest.MatcherAssert.assertThat;
-import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
-
-import java.util.Collections;
-import java.util.Map;
-import java.util.stream.Stream;
-
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
-import org.uniprot.core.json.parser.uniprot.UniProtKBEntryIT;
-import org.uniprot.core.parser.tsv.uniprot.UniProtKBEntryValueMapper;
-import org.uniprot.core.uniprotkb.UniProtKBEntry;
+import org.uniprot.core.json.parser.uniref.UniRefTest;
+import org.uniprot.core.parser.tsv.uniref.UniRefEntryValueMapper;
+import org.uniprot.core.uniref.UniRefEntry;
 import org.uniprot.core.util.Utils;
 import org.uniprot.store.config.UniProtDataType;
 import org.uniprot.store.config.returnfield.config.ReturnFieldConfig;
@@ -24,24 +15,32 @@ import org.uniprot.store.config.returnfield.model.ReturnField;
 import org.uniprot.store.config.searchfield.common.SearchFieldConfig;
 import org.uniprot.store.config.searchfield.factory.SearchFieldConfigFactory;
 
-/**
- * Created 17/03/20
- *
- * @author Edd
- */
-class UniProtKBReturnFieldConfigImplIT {
+import java.util.Collections;
+import java.util.Map;
+import java.util.stream.Stream;
 
-    private static UniProtKBEntry entry;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+/**
+ * @author lgonzales
+ * @since 2020-03-25
+ */
+class UniRefReturnFieldConfigImplIT {
+
+    private static UniRefEntry entry;
     private static ReturnFieldConfig returnFieldConfig;
     private static SearchFieldConfig searchFieldConfig;
 
     @BeforeAll
     static void setUp() {
         returnFieldConfig =
-                ReturnFieldConfigFactory.getReturnFieldConfig(UniProtDataType.UNIPROTKB);
+                ReturnFieldConfigFactory.getReturnFieldConfig(UniProtDataType.UNIREF);
         searchFieldConfig =
-                SearchFieldConfigFactory.getSearchFieldConfig(UniProtDataType.UNIPROTKB);
-        entry = UniProtKBEntryIT.getCompleteColumnsUniProtEntry();
+                SearchFieldConfigFactory.getSearchFieldConfig(UniProtDataType.UNIREF);
+        entry = UniRefTest.getCompleteUniRefEntry();
     }
 
     @ParameterizedTest(name = "Sort field [{0}] for return field exists?")
@@ -53,9 +52,10 @@ class UniProtKBReturnFieldConfigImplIT {
     @ParameterizedTest(name = "Return TSV column [{0}] for return field exists?")
     @MethodSource("provideReturnFieldNames")
     void validReturnFieldWithMappedEntryDefined(String returnFieldName) {
-        UniProtKBEntryValueMapper entityValueMapper = new UniProtKBEntryValueMapper();
+        UniRefEntryValueMapper entityValueMapper = new UniRefEntryValueMapper();
         Map<String, String> mappedField =
                 entityValueMapper.mapEntity(entry, Collections.singletonList(returnFieldName));
+        System.out.println(returnFieldName+" : "+mappedField.get(returnFieldName));
         assertNotNull(mappedField.get(returnFieldName));
         assertFalse(mappedField.get(returnFieldName).isEmpty());
     }
