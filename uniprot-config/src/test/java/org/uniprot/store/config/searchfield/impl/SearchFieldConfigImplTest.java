3c49b30a056709708692cefc8d2e953c28b20ca2
lgonzales <lgonzales@ebi.ac.uk>
2020-04-06 14:56:12 +0100

    fix test and code formatter

--- uniprot-config/src/test/java/org/uniprot/store/config/searchfield/impl/SearchFieldConfigImplTest.java
+++ uniprot-config/src/test/java/org/uniprot/store/config/searchfield/impl/SearchFieldConfigImplTest.java
@@ -31,7 +31,7 @@ public class SearchFieldConfigImplTest {
     void testSize() {
         List<SearchFieldItem> items = searchFieldConfig.getSearchFieldItems();
         Assertions.assertNotNull(items);
-        assertEquals(493, items.size());
+        assertEquals(494, items.size());
     }
 
     @Test
@@ -46,7 +46,7 @@ public class SearchFieldConfigImplTest {
                                         StringUtils.isNotEmpty(i.getFieldName())
                                                 && i.getFieldName().startsWith("xref_count_"))
                         .count();
-        Assertions.assertEquals(177, xrefCountFieldsCount);
+        Assertions.assertEquals(178, xrefCountFieldsCount);
     }
 
     @Test
