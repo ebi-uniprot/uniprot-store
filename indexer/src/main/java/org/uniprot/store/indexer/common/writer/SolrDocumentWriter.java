74946484954944d281f72349c0bb9b24da6c9f63
lgonzales <lgonzales@ebi.ac.uk>
2020-04-15 11:55:33 +0100

    Add logs for uniparc indexer

--- indexer/src/main/java/org/uniprot/store/indexer/common/writer/SolrDocumentWriter.java
+++ indexer/src/main/java/org/uniprot/store/indexer/common/writer/SolrDocumentWriter.java
@@ -1,13 +1,16 @@
 package org.uniprot.store.indexer.common.writer;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.batch.item.ItemWriter;
 import org.uniprot.store.indexer.common.config.UniProtSolrOperations;
 import org.uniprot.store.search.SolrCollection;
 import org.uniprot.store.search.document.Document;
 
 /** @author lgonzales */
+@Slf4j
 public class SolrDocumentWriter<T extends Document> implements ItemWriter<T> {
     private final UniProtSolrOperations solrOperations;
     private final SolrCollection collection;
@@ -19,7 +22,16 @@ public class SolrDocumentWriter<T extends Document> implements ItemWriter<T> {
 
     @Override
     public void write(List<? extends T> items) {
-        this.solrOperations.saveBeans(collection.name(), items);
-        this.solrOperations.softCommit(collection.name());
+        try {
+            this.solrOperations.saveBeans(collection.name(), items);
+            this.solrOperations.softCommit(collection.name());
+        }catch (Throwable error){
+            log.error("Error writing to solr: ", error);
+            String ids = items.stream()
+                    .map(Document::getDocumentId)
+                    .collect(Collectors.joining(", "));
+            log.warn("Failed document ids: "+ids);
+            throw error;
+        }
     }
 }
