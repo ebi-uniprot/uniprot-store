e6e27356f1e3f37b019d6624e5367593c945aabd
lgonzales <lgonzales@ebi.ac.uk>
2020-06-01 16:06:53 +0100

    Add Glygen DR line

--- uniprot-config/src/test/java/org/uniprot/store/config/searchfield/impl/SearchFieldConfigImplTest.java
+++ uniprot-config/src/test/java/org/uniprot/store/config/searchfield/impl/SearchFieldConfigImplTest.java
@@ -31,7 +31,7 @@ public class SearchFieldConfigImplTest {
     void testSize() {
         List<SearchFieldItem> items = searchFieldConfig.getSearchFieldItems();
         Assertions.assertNotNull(items);
-        assertEquals(497, items.size());
+        assertEquals(498, items.size());
     }
 
     @Test
@@ -46,7 +46,7 @@ public class SearchFieldConfigImplTest {
                                         StringUtils.isNotEmpty(i.getFieldName())
                                                 && i.getFieldName().startsWith("xref_count_"))
                         .count();
-        Assertions.assertEquals(181, xrefCountFieldsCount);
+        Assertions.assertEquals(182, xrefCountFieldsCount);
     }
 
     @Test
