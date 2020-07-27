78687b17550477f7e930b3f0247f742928ef12fc
Shadab Ahmad <sahmad@ebi.ac.uk>
2020-07-27 13:42:59 +0100

    index members of uniref100 using spring batch

--- uniprot-datastore/src/main/java/org/uniprot/store/datastore/member/uniref/UniRefMemberProcessor.java
+++ uniprot-datastore/src/main/java/org/uniprot/store/datastore/member/uniref/UniRefMemberProcessor.java
@@ -2,22 +2,49 @@ package org.uniprot.store.datastore.member.uniref;
 
 import org.springframework.batch.item.ItemProcessor;
 import org.uniprot.core.uniref.RepresentativeMember;
+import org.uniprot.core.uniref.UniRefMember;
+import org.uniprot.core.uniref.UniRefMemberIdType;
+import org.uniprot.core.uniref.impl.RepresentativeMemberBuilder;
 import org.uniprot.core.xml.jaxb.uniref.MemberType;
+import org.uniprot.core.xml.uniref.MemberConverter;
 import org.uniprot.core.xml.uniref.RepresentativeMemberConverter;
+
+import java.util.Objects;
 
 /**
  * @author sahmad
  * @since 23/07/2020
  */
 public class UniRefMemberProcessor implements ItemProcessor<MemberType, RepresentativeMember> {
-    private final RepresentativeMemberConverter converter;
+    private final RepresentativeMemberConverter repMemberConverter;
+    private final MemberConverter memberConverter;
 
     public UniRefMemberProcessor() {
-        converter = new RepresentativeMemberConverter();
+        repMemberConverter = new RepresentativeMemberConverter();
+        memberConverter = new MemberConverter();
     }
 
     @Override
     public RepresentativeMember process(MemberType memberType) throws Exception {
-        return converter.fromXml(memberType);
+        RepresentativeMemberBuilder builder;
+
+        if(Objects.nonNull(memberType.getSequence())) {
+            RepresentativeMember repMember = repMemberConverter.fromXml(memberType);
+            builder = RepresentativeMemberBuilder.from(repMember).memberId(getMemberId(repMember));
+        } else {
+            UniRefMember member = memberConverter.fromXml(memberType);
+            builder = RepresentativeMemberBuilder.from(member).memberId(getMemberId(member));
+        }
+        // update seed to null
+        return builder.isSeed(null).build();
+    }
+
+    // get accession id if memberType is UniRefMemberIdType.UNIPROTKB
+    private String getMemberId(UniRefMember member) {
+        if (member.getMemberIdType() == UniRefMemberIdType.UNIPARC) {
+            return member.getMemberId();
+        } else {
+            return member.getUniProtAccessions().get(0).getValue();
+        }
     }
 }
