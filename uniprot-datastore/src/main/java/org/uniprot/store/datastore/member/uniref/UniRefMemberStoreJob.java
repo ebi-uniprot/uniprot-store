1f348482d81dc32355efc038de007aa8fd72ae0a
Shadab Ahmad <sahmad@ebi.ac.uk>
2020-07-30 14:28:22 +0100

    refactor the code little bit

--- uniprot-datastore/src/main/java/org/uniprot/store/datastore/member/uniref/UniRefMemberStoreJob.java
+++ uniprot-datastore/src/main/java/org/uniprot/store/datastore/member/uniref/UniRefMemberStoreJob.java
@@ -1,18 +1,25 @@
 package org.uniprot.store.datastore.member.uniref;
 
-import static org.uniprot.store.datastore.utils.Constants.UNIREF_MEMBER_STORE_JOB;
-
+import org.springframework.aop.framework.Advised;
+import org.springframework.aop.support.AopUtils;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.item.ItemProcessor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.uniprot.core.uniref.RepresentativeMember;
+import org.uniprot.core.xml.jaxb.uniref.MemberType;
+import org.uniprot.store.datastore.UniProtStoreClient;
 import org.uniprot.store.datastore.member.uniref.config.UniRefMemberStoreProperties;
 import org.uniprot.store.job.common.listener.LogRateListener;
 import org.uniprot.store.job.common.listener.WriteRetrierLogJobListener;
+
+import java.util.List;
+
+import static org.uniprot.store.datastore.utils.Constants.UNIREF_MEMBER_STORE_JOB;
 
 /**
  * @author sahmad
@@ -50,5 +57,26 @@ public class UniRefMemberStoreJob {
     @Bean(name = "unirefMemberLogRateListener")
     public LogRateListener<RepresentativeMember> unirefMemberLogRateListener() {
         return new LogRateListener<>(unirefMemberStoreProperties.getLogRateInterval());
+    }
+
+    @Bean(name = "uniref90And50MemberLogRateListener")
+    public LogRateListener<List<RepresentativeMember>> uniref90And50MemberLogRateListener() {
+        return new UniRef90And50MemberLogRateListener(
+                unirefMemberStoreProperties.getLogRateInterval());
+    }
+
+    // ---------------------- Processors ----------------------
+    @Bean(name = "uniref90And50MemberProcessor")
+    public ItemProcessor<List<MemberType>, List<RepresentativeMember>> uniref90And50MemberProcessor(
+            UniProtStoreClient<RepresentativeMember> unirefMemberStoreClient) {
+        return new UniRef90And50MemberProcessor(unirefMemberStoreClient);
+    }
+
+    public static Object unwrapProxy(Object bean) throws Exception {
+        if (AopUtils.isAopProxy(bean) && bean instanceof Advised) {
+            Advised advised = (Advised) bean;
+            bean = advised.getTargetSource().getTarget();
+        }
+        return bean;
     }
 }
