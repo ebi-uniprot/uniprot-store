44be86ac8a71db37ab307374ed59a5a2bda00868
Shadab Ahmad <sahmad@ebi.ac.uk>
2020-08-20 12:25:08 +0100

    fix test cases

--- uniprot-config/src/test/java/org/uniprot/store/config/uniparc/UniParcConfigUtilTest.java
+++ uniprot-config/src/test/java/org/uniprot/store/config/uniparc/UniParcConfigUtilTest.java
@@ -11,37 +11,32 @@ import org.junit.jupiter.params.provider.MethodSource;
 import org.uniprot.core.uniparc.UniParcDatabase;
 
 /**
- *
  * @author jluo
  * @date: 19-Aug-2020
- *
-*/
-
+ */
 public class UniParcConfigUtilTest {
-	
+
     @ParameterizedTest
     @MethodSource("setData")
-	void uniparcDatabaseToSearchField(UniParcDatabase db, String name, String value ) {
-		Map.Entry<String, String> entry =  UniParcConfigUtil.uniparcDatabaseToSearchField(db);
-		Assertions.assertEquals(name, entry.getKey());
-		Assertions.assertEquals(value, entry.getValue());
-	}
-   
-	
-	private static List<Arguments> setData() {
-		List<Arguments> result= new ArrayList<>();
-		for(UniParcDatabase db: UniParcDatabase.values()) {
-			if(db ==UniParcDatabase.EMBL ) {
-				result.add(Arguments.of(db, "EMBL CDS", "embl-cds"));
-			}else if ((db ==UniParcDatabase.SWISSPROT)|| (db ==UniParcDatabase.TREMBL)) {
-				result.add(Arguments.of(db, "UniProtKB", "Uniprot"));
-			}else if(db ==UniParcDatabase.SWISSPROT_VARSPLIC ) {
-				result.add(Arguments.of(db, "UniProtKB/Swiss-Prot isoforms", "isoforms"));
-			}else {
-				result.add(Arguments.of(db, db.getDisplayName(), db.getDisplayName()));
-			}
-		}
-		return result;
+    void uniparcDatabaseToSearchField(UniParcDatabase db, String name, String value) {
+        Map.Entry<String, String> entry = UniParcConfigUtil.uniparcDatabaseToSearchField(db);
+        Assertions.assertEquals(name, entry.getKey());
+        Assertions.assertEquals(value, entry.getValue());
+    }
+
+    private static List<Arguments> setData() {
+        List<Arguments> result = new ArrayList<>();
+        for (UniParcDatabase db : UniParcDatabase.values()) {
+            if (db == UniParcDatabase.EMBL) {
+                result.add(Arguments.of(db, "EMBL CDS", "embl-cds"));
+            } else if ((db == UniParcDatabase.SWISSPROT) || (db == UniParcDatabase.TREMBL)) {
+                result.add(Arguments.of(db, "UniProtKB", "Uniprot"));
+            } else if (db == UniParcDatabase.SWISSPROT_VARSPLIC) {
+                result.add(Arguments.of(db, "UniProtKB/Swiss-Prot isoforms", "isoforms"));
+            } else {
+                result.add(Arguments.of(db, db.getDisplayName(), db.getDisplayName()));
+            }
+        }
+        return result;
     }
 }
-
