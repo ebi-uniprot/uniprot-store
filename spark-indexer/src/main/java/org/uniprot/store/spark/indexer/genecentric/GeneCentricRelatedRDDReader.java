153d393e128a0ebd5d118d0b327efc8b4a4bef80
lgonzales <lgonzales@ebi.ac.uk>
2020-11-09 19:27:25 +0000

    fix sonar

--- spark-indexer/src/main/java/org/uniprot/store/spark/indexer/genecentric/GeneCentricRelatedRDDReader.java
+++ spark-indexer/src/main/java/org/uniprot/store/spark/indexer/genecentric/GeneCentricRelatedRDDReader.java
@@ -4,50 +4,32 @@ import static org.uniprot.store.spark.indexer.common.util.SparkUtils.getInputRel
 
 import java.util.ResourceBundle;
 
-import org.apache.hadoop.io.LongWritable;
-import org.apache.hadoop.io.Text;
-import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
-import org.apache.spark.api.java.JavaNewHadoopRDD;
-import org.apache.spark.api.java.JavaPairRDD;
-import org.apache.spark.api.java.JavaSparkContext;
-import org.uniprot.core.genecentric.GeneCentricEntry;
 import org.uniprot.store.spark.indexer.common.JobParameter;
-import org.uniprot.store.spark.indexer.common.reader.PairRDDReader;
+import org.uniprot.store.spark.indexer.genecentric.mapper.FastaToGeneCentricEntry;
 import org.uniprot.store.spark.indexer.genecentric.mapper.FastaToRelatedGeneCentricEntry;
 
 /**
  * @author lgonzales
  * @since 20/10/2020
  */
-public class GeneCentricRelatedRDDReader implements PairRDDReader<String, GeneCentricEntry> {
+public class GeneCentricRelatedRDDReader extends GeneCentricRDDReader {
 
     private final JobParameter jobParameter;
 
     public GeneCentricRelatedRDDReader(JobParameter jobParameter) {
+        super(jobParameter);
         this.jobParameter = jobParameter;
     }
 
-    private static final String SPLITTER = "\n>";
-
-    /** @return an JavaPairRDD with <accession, GeneCentricEntry> */
     @Override
-    public JavaPairRDD<String, GeneCentricEntry> load() {
-        ResourceBundle config = jobParameter.getApplicationConfig();
-        JavaSparkContext jsc = jobParameter.getSparkContext();
-        String releaseInputDir = getInputReleaseDirPath(config, jobParameter.getReleaseName());
-        String filePath = releaseInputDir + config.getString("genecentric.related.fasta.files");
-        jsc.hadoopConfiguration().set("textinputformat.record.delimiter", SPLITTER);
+    public FastaToGeneCentricEntry getFastaMapper() {
+        return new FastaToRelatedGeneCentricEntry();
+    }
 
-        JavaPairRDD<LongWritable, Text> javaPairRDD =
-                jsc.newAPIHadoopFile(
-                        filePath,
-                        TextInputFormat.class,
-                        LongWritable.class,
-                        Text.class,
-                        jsc.hadoopConfiguration());
-        JavaNewHadoopRDD<LongWritable, Text> hadoopRDD = (JavaNewHadoopRDD) javaPairRDD;
-        return hadoopRDD
-                .mapPartitionsWithInputSplit(new FastaToRelatedGeneCentricEntry(), true)
-                .mapToPair(tuple -> tuple);
+    @Override
+    public String getFastaFilePath() {
+        ResourceBundle config = jobParameter.getApplicationConfig();
+        String releaseInputDir = getInputReleaseDirPath(config, jobParameter.getReleaseName());
+        return releaseInputDir + config.getString("genecentric.related.fasta.files");
     }
 }
