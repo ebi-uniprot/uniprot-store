69285fd34985f421c9c8e2141a254f693ecb04d4
eddturner <eddturner@gmai.com>
2020-12-02 16:01:37 +0000

    adding publication mapper code

---
+++ common-job/src/main/java/org/uniprot/store/reader/publications/RawMappedReference.java
@@ -0,0 +1,17 @@
+package org.uniprot.store.reader.publications;
+
+import java.util.List;
+
+/**
+ * Created 02/12/2020
+ *
+ * @author Edd
+ */
+public class RawMappedReference {
+    String accession;
+    String source;
+    String sourceId;
+    String pubMedId;
+    List<String> categories;
+    String annotation;
+}
