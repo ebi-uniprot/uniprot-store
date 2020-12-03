1059c0917586e3984bbd5f226b30e2792a3fd046
eddturner <eddturner@gmai.com>
2020-12-03 15:42:46 +0000

    convert mapped references

--- common-job/src/main/java/org/uniprot/store/reader/publications/ComputationallyMappedReferenceMapper.java
+++ common-job/src/main/java/org/uniprot/store/reader/publications/ComputationallyMappedReferenceMapper.java
@@ -2,6 +2,7 @@ package org.uniprot.store.reader.publications;
 
 import org.uniprot.core.publication.ComputationallyMappedReference;
 import org.uniprot.core.publication.impl.ComputationallyMappedReferenceBuilder;
+import org.uniprot.core.publication.impl.MappedSourceBuilder;
 
 /**
  * Created 02/12/2020
@@ -14,8 +15,11 @@ public class ComputationallyMappedReferenceMapper
     ComputationallyMappedReference convertRawMappedReference(RawMappedReference reference) {
         return new ComputationallyMappedReferenceBuilder()
                 .uniProtKBAccession(reference.accession)
-                .source(reference.source)
-                .sourceId(reference.sourceId)
+                .sourcesAdd(
+                        new MappedSourceBuilder()
+                                .source(reference.source)
+                                .sourceIdsAdd(reference.sourceId)
+                                .build())
                 .pubMedId(reference.pubMedId)
                 .sourceCategoriesSet(reference.categories)
                 .annotation(reference.annotation)
