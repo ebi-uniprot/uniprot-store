ed72a9293d620cb342b738c6ac24a5443a4666e0
eddturner <eddturner@gmai.com>
2020-12-09 16:03:53 +0000

    added basic reader and processor

---
+++ indexer/src/main/java/org/uniprot/store/indexer/publication/community/CommunityPublicationItemReader.java
@@ -0,0 +1,28 @@
+package org.uniprot.store.indexer.publication.community;
+
+import org.springframework.batch.item.ItemReader;
+import org.uniprot.core.publication.CommunityMappedReference;
+import org.uniprot.store.reader.publications.CommunityMappedReferenceConverter;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Iterator;
+
+public class CommunityPublicationItemReader implements ItemReader<CommunityMappedReference> {
+    private final Iterator<String> lines;
+    private final CommunityMappedReferenceConverter converter;
+
+    public CommunityPublicationItemReader(String filePath) throws IOException {
+        lines = Files.lines(Paths.get(filePath)).iterator();
+        converter = new CommunityMappedReferenceConverter();
+    }
+
+    @Override
+    public CommunityMappedReference read() {
+        if (lines.hasNext()) {
+            return converter.convert(this.lines.next());
+        }
+        return null;
+    }
+}
