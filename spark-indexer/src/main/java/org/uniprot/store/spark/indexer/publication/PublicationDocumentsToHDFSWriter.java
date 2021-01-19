679199862e999b920cde4283c77923c1bf793d7c
eddturner <eddturner@gmai.com>
2021-01-19 15:01:54 +0000

    created Solr document writer to HDFS for {UniProtKB publications, computational references, community references}

--- spark-indexer/src/main/java/org/uniprot/store/spark/indexer/publication/PublicationDocumentsToHDFSWriter.java
+++ spark-indexer/src/main/java/org/uniprot/store/spark/indexer/publication/PublicationDocumentsToHDFSWriter.java
@@ -1,29 +1,40 @@
 package org.uniprot.store.spark.indexer.publication;
 
-import static org.uniprot.store.spark.indexer.common.util.SparkUtils.getCollectionOutputReleaseDirPath;
-
-import java.util.ResourceBundle;
-
 import lombok.extern.slf4j.Slf4j;
-
 import org.apache.spark.api.java.JavaPairRDD;
 import org.apache.spark.api.java.JavaRDD;
-import org.uniprot.core.uniprotkb.UniProtKBEntry;
+import org.apache.spark.api.java.JavaSparkContext;
+import org.uniprot.core.publication.MappedReference;
+import org.uniprot.store.indexer.publication.common.PublicationUtils;
+import org.uniprot.store.reader.publications.CommunityMappedReferenceConverter;
+import org.uniprot.store.reader.publications.ComputationallyMappedReferenceConverter;
+import org.uniprot.store.reader.publications.MappedReferenceConverter;
 import org.uniprot.store.search.SolrCollection;
 import org.uniprot.store.search.document.publication.PublicationDocument;
 import org.uniprot.store.spark.indexer.common.JobParameter;
 import org.uniprot.store.spark.indexer.common.util.SolrUtils;
 import org.uniprot.store.spark.indexer.common.writer.DocumentsToHDFSWriter;
-import org.uniprot.store.spark.indexer.publication.mapper.UniProtKBEntryToPublicationDocuments;
+import org.uniprot.store.spark.indexer.publication.mapper.MappedReferencesToPublicationDocumentConverter;
+import org.uniprot.store.spark.indexer.publication.mapper.UniProtKBPublicationToMappedReference;
 import org.uniprot.store.spark.indexer.uniprot.UniProtKBRDDTupleReader;
+import scala.Tuple2;
+
+import java.util.ResourceBundle;
+
+import static org.uniprot.store.spark.indexer.common.util.SparkUtils.getCollectionOutputReleaseDirPath;
+import static org.uniprot.store.spark.indexer.common.util.SparkUtils.getInputReleaseDirPath;
 
 /**
- * @author sahmad
- * @created 15/12/2020
+ * The purpose of this class is to load all publication sources (UniProtKB references, PIR
+ * computationally mapped references, and community mapped publications), join their data to create
+ * {@link PublicationDocument}s, and write them to HDFS.
+ *
+ * <p>Created 19/01/2021
+ *
+ * @author Edd
  */
 @Slf4j
 public class PublicationDocumentsToHDFSWriter implements DocumentsToHDFSWriter {
-
     private final JobParameter parameter;
     private final ResourceBundle config;
     private final String releaseName;
@@ -36,12 +47,84 @@ public class PublicationDocumentsToHDFSWriter implements DocumentsToHDFSWriter {
 
     @Override
     public void writeIndexDocumentsToHDFS() {
-        UniProtKBRDDTupleReader uniProtKBReader = new UniProtKBRDDTupleReader(parameter, false);
-        JavaPairRDD<String, UniProtKBEntry> uniProtKBRDD = uniProtKBReader.load();
-        JavaRDD<PublicationDocument> publicationDocumentsRDD =
-                uniProtKBRDD.flatMapValues(new UniProtKBEntryToPublicationDocuments()).values();
-        saveToHDFS(publicationDocumentsRDD);
-        log.info("Completed UniProtKB publication document prepare Solr index");
+        // load UniProtKB mapped references
+        JavaPairRDD<String, MappedReference> kbMappedRefsRDD = loadUniProtKBMappedRefs();
+
+        // load computational docs
+        JavaPairRDD<String, MappedReference> computationalMappedRefsRDD = loadComputationalDocs();
+
+        // load community docs
+        JavaPairRDD<String, MappedReference> communityMappedRefsRDD = loadCommunityDocs();
+
+        // at this stage there will be duplicated keys
+        JavaPairRDD<String, MappedReference> allMappedRefs =
+                kbMappedRefsRDD.union(computationalMappedRefsRDD).union(communityMappedRefsRDD);
+
+        // group records with identical keys, so that the record value is an iterable
+        // of MappedReferences. Then convert this iterable into a PublicationDocument.
+        JavaRDD<PublicationDocument> pubDocRDD =
+                allMappedRefs
+                        .groupByKey()
+                        .map(new MappedReferencesToPublicationDocumentConverter());
+
+        saveToHDFS(pubDocRDD);
+
+        log.info("Completed writing UniProtKB publication documents to HDFS");
+    }
+
+    public static String getJoinKey(String accession, String pubMed) {
+        String refIdentifier = pubMed;
+        if (refIdentifier == null) {
+            refIdentifier = "NO-PUBMED" + PublicationUtils.getDocumentId();
+        }
+
+        return accession + "_" + refIdentifier;
+    }
+
+    public static String[] separateJoinKey(String joinKey) {
+        String[] parts = joinKey.split("_");
+        if (parts[1].startsWith("NO-PUBMED")) {
+            parts[1] = null;
+        }
+        return parts;
+    }
+
+    private JavaPairRDD<String, MappedReference> loadUniProtKBMappedRefs() {
+        UniProtKBRDDTupleReader uniProtKBReader =
+                new UniProtKBRDDTupleReader(this.parameter, false);
+        JavaRDD<String> uniProtKBEntryStringsRDD = uniProtKBReader.loadFlatFileToRDD();
+
+        return uniProtKBEntryStringsRDD.flatMapToPair(
+                new UniProtKBPublicationToMappedReference());
+    }
+
+    private JavaPairRDD<String, MappedReference> loadComputationalDocs() {
+        return loadMappedReferenceRDD(
+                "computational.mapped.references.file",
+                new ComputationallyMappedReferenceConverter());
+    }
+
+    private JavaPairRDD<String, MappedReference> loadCommunityDocs() {
+        return loadMappedReferenceRDD(
+                "community.mapped.references.file", new CommunityMappedReferenceConverter());
+    }
+
+    private JavaPairRDD<String, MappedReference> loadMappedReferenceRDD(
+            String srcFilePathProperty, MappedReferenceConverter<?> converter) {
+        JavaSparkContext jsc = this.parameter.getSparkContext();
+        String releaseInputDir = getInputReleaseDirPath(config, this.parameter.getReleaseName());
+        String filePath = releaseInputDir + config.getString(srcFilePathProperty);
+        JavaRDD<String> rawMappedRefStrRdd = jsc.textFile(filePath);
+
+        return rawMappedRefStrRdd
+                .map(converter::convert)
+                .mapToPair(
+                        ref ->
+                                new Tuple2<>(
+                                        getJoinKey(
+                                                ref.getUniProtKBAccession().getValue(),
+                                                ref.getPubMedId()),
+                                        ref));
     }
 
     void saveToHDFS(JavaRDD<PublicationDocument> publicationDocumentsRDD) {
---
+++ spark-indexer/src/main/java/org/uniprot/store/spark/indexer/publication/mapper/UniProtKBPublicationToMappedReference.java
@@ -0,0 +1,263 @@
+package org.uniprot.store.spark.indexer.publication.mapper;
+
+import org.apache.spark.api.java.function.PairFlatMapFunction;
+import org.uniprot.core.flatfile.parser.UniprotKBLineParser;
+import org.uniprot.core.flatfile.parser.impl.DefaultUniprotKBLineParserFactory;
+import org.uniprot.core.flatfile.parser.impl.ac.AcLineObject;
+import org.uniprot.core.flatfile.parser.impl.entry.EntryObject;
+import org.uniprot.core.flatfile.parser.impl.entry.ReferenceObjectConverter;
+import org.uniprot.core.flatfile.parser.impl.ra.RaLineObject;
+import org.uniprot.core.flatfile.parser.impl.rc.RcLineObject;
+import org.uniprot.core.flatfile.parser.impl.rg.RgLineObject;
+import org.uniprot.core.flatfile.parser.impl.rl.RlLineObject;
+import org.uniprot.core.flatfile.parser.impl.rn.RnLineObject;
+import org.uniprot.core.flatfile.parser.impl.rp.RpLineObject;
+import org.uniprot.core.flatfile.parser.impl.rt.RtLineObject;
+import org.uniprot.core.flatfile.parser.impl.rx.RxLineObject;
+import org.uniprot.core.flatfile.writer.LineType;
+import org.uniprot.core.publication.MappedReference;
+import org.uniprot.core.uniprotkb.UniProtKBEntryType;
+import org.uniprot.core.uniprotkb.UniProtKBReference;
+import org.uniprot.store.indexer.uniprotkb.converter.UniProtEntryReferencesConverter;
+import org.uniprot.store.search.document.publication.PublicationDocument;
+import scala.Tuple2;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
+
+import static org.uniprot.store.spark.indexer.publication.PublicationDocumentsToHDFSWriter.getJoinKey;
+
+/**
+ * Maps an entry string to an iterator of tuples with values <accession, MappedReference>.
+ *
+ * <p>Created 18/01/2021
+ *
+ * @author Edd
+ */
+public class UniProtKBPublicationToMappedReference
+        implements PairFlatMapFunction<String, String, MappedReference> {
+
+    private static final Pattern REVIEWED_REGEX = Pattern.compile("^ID .*Reviewed.*");
+    private static final Pattern REFERENCE_REGEX = Pattern.compile("^(RN|RA|RC|RG|RL|RP|RT|RX) .*");
+    private final UniProtEntryReferencesConverter referencesConverter =
+            new UniProtEntryReferencesConverter();
+
+    @Override
+    public Iterator<Tuple2<String, MappedReference>> call(String entryStr) throws Exception {
+        String[] lines = entryStr.split("\n");
+
+        String accession = getAccession(lines);
+        UniProtKBEntryType entryType = getEntryType(lines);
+        List<UniProtKBReference> references = createReferences(lines);
+
+        AtomicInteger refNumberCounter = new AtomicInteger();
+        return references.stream()
+                .map(
+                        ref ->
+                                createMappedReferenceInfo(
+                                        accession,
+                                        entryType,
+                                        ref,
+                                        refNumberCounter.getAndIncrement()))
+                .map(
+                        referenceInfo ->
+                                new Tuple2<>(
+                                        getJoinKey(accession, referenceInfo.pubMed),
+                                        referenceInfo.mappedReference))
+                .iterator();
+    }
+
+    String getAccession(String[] lines) {
+        final UniprotKBLineParser<AcLineObject> acParser =
+                new DefaultUniprotKBLineParserFactory().createAcLineParser();
+
+        StringBuilder sb = new StringBuilder();
+        boolean foundAccession = false;
+        for (String line : lines) {
+            boolean lineStartsWithAC = line.startsWith("AC ");
+            if (lineStartsWithAC) {
+                sb.append(line).append('\n');
+                foundAccession = true;
+            }
+            if (foundAccession && !lineStartsWithAC) {
+                break;
+            }
+        }
+
+        return acParser.parse(sb.toString() + "\n").primaryAcc;
+    }
+
+    List<UniProtKBReference> createReferences(String[] lines) {
+        // for each reference in the entry, create a list of lines related to it
+        List<List<String>> rawRefs = new ArrayList<>();
+        List<String> rawRef = new ArrayList<>();
+        for (String line : lines) {
+            if (REFERENCE_REGEX.matcher(line).matches()) {
+                if (line.startsWith("RN ")) {
+                    if (!rawRef.isEmpty()) {
+                        rawRefs.add(rawRef);
+                    }
+                    rawRef = new ArrayList<>();
+                }
+
+                rawRef.add(line + "\n");
+            }
+        }
+
+        rawRefs.add(rawRef);
+
+        return rawRefs.stream()
+                .map(this::getSections)
+                .map(this::createReference)
+                .collect(Collectors.toList());
+    }
+
+    List<LineTypeSection> getSections(List<String> lines) {
+        List<LineTypeSection> lineTypeSections = new ArrayList<>();
+
+        boolean isNotLastSection = true;
+        int offset = 0;
+        while (isNotLastSection) {
+            LineTypeSection section = getSectionWithSameLineType(lines, offset);
+            lineTypeSections.add(section);
+            isNotLastSection = section.isNotLastSection;
+            offset = section.lineNumber;
+        }
+
+        return lineTypeSections;
+    }
+
+    LineTypeSection getSectionWithSameLineType(List<String> lines, int offset) {
+        LineTypeSection lineTypeSection = new LineTypeSection();
+
+        LineType lineType = null;
+        StringBuilder sb = new StringBuilder();
+        int i = offset;
+        for (; i < lines.size(); i++) {
+            String line = lines.get(i);
+
+            lineType = LineType.valueOf(lines.get(i).substring(0, 2));
+            if (lineTypeSection.type == null) {
+                lineTypeSection.type = lineType;
+            } else if (lineType != lineTypeSection.type) {
+                break;
+            }
+
+            sb.append(line);
+        }
+
+        lineTypeSection.lineNumber = i;
+        lineTypeSection.value = sb.toString();
+        if (i == lines.size()) {
+            lineTypeSection.isNotLastSection = false;
+        }
+
+        return lineTypeSection;
+    }
+
+    static class MappedReferenceInfo {
+        MappedReference mappedReference;
+        String pubMed;
+    }
+
+    MappedReferenceInfo createMappedReferenceInfo(
+            String accession,
+            UniProtKBEntryType entryType,
+            UniProtKBReference reference,
+            int referenceNumber) {
+
+        String pubMed = referencesConverter.extractPubMed(reference.getCitation());
+
+        MappedReferenceInfo mappedReferenceInfo = new MappedReferenceInfo();
+        mappedReferenceInfo.mappedReference =
+                referencesConverter.createUniProtKBMappedReference(
+                        accession, entryType, reference, pubMed, referenceNumber);
+        mappedReferenceInfo.pubMed = pubMed;
+        return mappedReferenceInfo;
+    }
+
+    PublicationDocument.PublicationDocumentBuilder referenceToPubDocBuilder(
+            String accession,
+            UniProtKBEntryType entryType,
+            UniProtKBReference reference,
+            int referenceNumber) {
+        return referencesConverter.referenceToPublicationDocumentBuilder(
+                accession, entryType, reference, referenceNumber);
+    }
+
+    UniProtKBReference createReference(List<LineTypeSection> sections) {
+        final UniprotKBLineParser<RaLineObject> raLineParser =
+                new DefaultUniprotKBLineParserFactory().createRaLineParser();
+        final UniprotKBLineParser<RcLineObject> rcLineParser =
+                new DefaultUniprotKBLineParserFactory().createRcLineParser();
+        final UniprotKBLineParser<RgLineObject> rgLineParser =
+                new DefaultUniprotKBLineParserFactory().createRgLineParser();
+        final UniprotKBLineParser<RlLineObject> rlLineParser =
+                new DefaultUniprotKBLineParserFactory().createRlLineParser();
+        final UniprotKBLineParser<RnLineObject> rnLineParser =
+                new DefaultUniprotKBLineParserFactory().createRnLineParser();
+        final UniprotKBLineParser<RpLineObject> rpLineParser =
+                new DefaultUniprotKBLineParserFactory().createRpLineParser();
+        final UniprotKBLineParser<RtLineObject> rtLineParser =
+                new DefaultUniprotKBLineParserFactory().createRtLineParser();
+        final UniprotKBLineParser<RxLineObject> rxLineParser =
+                new DefaultUniprotKBLineParserFactory().createRxLineParser();
+
+        ReferenceObjectConverter referenceObjectConverter = new ReferenceObjectConverter();
+        EntryObject.ReferenceObject referenceObject = new EntryObject.ReferenceObject();
+
+        for (LineTypeSection section : sections) {
+            switch (section.type) {
+                case RA:
+                    referenceObject.ra = raLineParser.parse(section.value);
+                    break;
+                case RC:
+                    referenceObject.rc = rcLineParser.parse(section.value);
+                    break;
+                case RG:
+                    referenceObject.rg = rgLineParser.parse(section.value);
+                    break;
+                case RL:
+                    referenceObject.rl = rlLineParser.parse(section.value);
+                    break;
+                case RN:
+                    referenceObject.rn = rnLineParser.parse(section.value);
+                    break;
+                case RP:
+                    referenceObject.rp = rpLineParser.parse(section.value);
+                    break;
+                case RT:
+                    referenceObject.rt = rtLineParser.parse(section.value);
+                    break;
+                case RX:
+                    referenceObject.rx = rxLineParser.parse(section.value);
+                    break;
+                default:
+                    throw new IllegalStateException(
+                            "Unexpected line type encountered: " + section.value);
+            }
+        }
+
+        return referenceObjectConverter.convert(referenceObject);
+    }
+
+    private UniProtKBEntryType getEntryType(String[] lines) {
+        if (REVIEWED_REGEX.matcher(lines[0]).matches()) {
+            return UniProtKBEntryType.SWISSPROT;
+        } else {
+            return UniProtKBEntryType.TREMBL;
+        }
+    }
+
+    /** Represents all lines of the same type (e.g., RT) of a UniProtKB entry flatfile. */
+    static class LineTypeSection {
+        int lineNumber = 0;
+        LineType type;
+        String value;
+        boolean isNotLastSection = true;
+    }
+}
