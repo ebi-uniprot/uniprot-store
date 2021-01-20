0edaaff9d691972b7cb76428961f9f49093e2ca7
eddturner <eddturner@gmai.com>
2021-01-20 14:59:24 +0000

    Test UniProtKB/Community/Computational indexing of Solr documents to HDFS

---
+++ spark-indexer/src/main/java/org/uniprot/store/spark/indexer/publication/mapper/SparkComputationallyMappedReferenceConverter.java
@@ -0,0 +1,18 @@
+package org.uniprot.store.spark.indexer.publication.mapper;
+
+import org.apache.spark.api.java.function.Function;
+import org.uniprot.core.publication.MappedReference;
+import org.uniprot.store.reader.publications.ComputationallyMappedReferenceConverter;
+
+/**
+ * Created 19/01/2021
+ *
+ * @author Edd
+ */
+public class SparkComputationallyMappedReferenceConverter implements Function<String, MappedReference> {
+    @Override
+    public MappedReference call(String rawReference) throws Exception {
+        ComputationallyMappedReferenceConverter converter = new ComputationallyMappedReferenceConverter();
+        return converter.convert(rawReference);
+    }
+}
