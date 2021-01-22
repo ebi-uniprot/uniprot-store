b8a8887fbb1329d38a698a741dd2ebb76e5e7da1
eddturner <eddturner@gmai.com>
2021-01-22 15:37:34 +0000

    quick fix to see if serialization issue is resolved

--- spark-indexer/src/main/java/org/uniprot/store/spark/indexer/common/writer/SolrIndexWriter.java
+++ spark-indexer/src/main/java/org/uniprot/store/spark/indexer/common/writer/SolrIndexWriter.java
@@ -1,21 +1,20 @@
 package org.uniprot.store.spark.indexer.common.writer;
 
-import static java.util.Collections.singletonList;
-
-import java.time.Duration;
-import java.util.Iterator;
-import java.util.Optional;
-
 import lombok.extern.slf4j.Slf4j;
 import net.jodah.failsafe.Failsafe;
 import net.jodah.failsafe.RetryPolicy;
-
 import org.apache.solr.client.solrj.SolrClient;
 import org.apache.solr.client.solrj.SolrServerException;
 import org.apache.solr.client.solrj.impl.CloudSolrClient;
 import org.apache.solr.common.SolrInputDocument;
 import org.apache.spark.api.java.function.VoidFunction;
 import org.uniprot.store.spark.indexer.common.exception.SolrIndexException;
+
+import java.time.Duration;
+import java.util.Iterator;
+import java.util.Optional;
+
+import static java.util.Collections.singletonList;
 
 /**
  * @author lgonzales
@@ -26,20 +25,27 @@ public class SolrIndexWriter implements VoidFunction<Iterator<SolrInputDocument>
 
     private static final long serialVersionUID = 1997175675889081522L;
     protected final SolrIndexParameter parameter;
-    protected final RetryPolicy<Object> retryPolicy;
+//    protected final RetryPolicy<Object> retryPolicy;
 
     public SolrIndexWriter(SolrIndexParameter parameter) {
         this.parameter = parameter;
-        this.retryPolicy =
+//        this.retryPolicy =
+//                new RetryPolicy<>()
+//                        .handle(SolrServerException.class)
+//                        .withDelay(Duration.ofMillis(parameter.getDelay()))
+//                        .onFailedAttempt(e -> log.warn("solr save attempt failed"))
+//                        .withMaxRetries(parameter.getMaxRetry());
+    }
+
+    @Override
+    public void call(Iterator<SolrInputDocument> docs) throws Exception {
+        RetryPolicy<Object> retryPolicy =
                 new RetryPolicy<>()
                         .handle(SolrServerException.class)
                         .withDelay(Duration.ofMillis(parameter.getDelay()))
                         .onFailedAttempt(e -> log.warn("solr save attempt failed"))
                         .withMaxRetries(parameter.getMaxRetry());
-    }
 
-    @Override
-    public void call(Iterator<SolrInputDocument> docs) throws Exception {
         try (SolrClient client = getSolrClient()) {
             Failsafe.with(retryPolicy).run(() -> client.add(parameter.getCollectionName(), docs));
         } catch (Exception e) {
