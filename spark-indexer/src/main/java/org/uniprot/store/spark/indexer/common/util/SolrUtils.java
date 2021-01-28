2fed1b44dd97e864916ca28f28859d469a600410
eddturner <eddturner@gmai.com>
2021-01-28 14:42:59 +0000

    testing

--- spark-indexer/src/main/java/org/uniprot/store/spark/indexer/common/util/SolrUtils.java
+++ spark-indexer/src/main/java/org/uniprot/store/spark/indexer/common/util/SolrUtils.java
@@ -1,11 +1,6 @@
 package org.uniprot.store.spark.indexer.common.util;
 
-import static java.util.Collections.*;
-
-import java.util.Optional;
-
 import lombok.extern.slf4j.Slf4j;
-
 import org.apache.solr.client.solrj.beans.DocumentObjectBinder;
 import org.apache.solr.client.solrj.impl.CloudSolrClient;
 import org.apache.solr.common.SolrInputDocument;
@@ -13,6 +8,16 @@ import org.apache.spark.api.java.JavaPairRDD;
 import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.rdd.RDD;
 import org.uniprot.store.search.document.Document;
+import scala.Tuple2;
+
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.StreamSupport;
+
+import static java.util.Arrays.asList;
+import static java.util.Collections.singletonList;
 
 /**
  * @author lgonzales
@@ -33,8 +38,30 @@ public class SolrUtils {
         RDD<SolrInputDocument> solrInputDocumentRDD =
                 docRDD.map(SolrUtils::convertToSolrInputDocument).rdd();
         log.info("Document count to save on HDFS: {}", solrInputDocumentRDD.count());
-        solrInputDocumentRDD.saveAsObjectFile(savePath);
+        long idCount =
+                solrInputDocumentRDD
+                        .toJavaRDD()
+                        .mapToPair(x -> new Tuple2<>(x.getField("id"), x))
+                        .groupByKey()
+                        .count();
+        log.info("Document ID count to save on HDFS: {}", idCount);
+        solrInputDocumentRDD
+                .toJavaRDD()
+                .mapToPair(x -> new Tuple2<>(x.getField("accession").getFirstValue(), x))
+                .groupByKey()
+                .mapValues(f -> StreamSupport.stream(f.spliterator(), false).count())
+                .filter(x -> x._2 > 1000)
+                .take(200)
+                .forEach(x -> log.info("accession:{}, count:{}", x._1, x._2));
+
+        //        solrInputDocumentRDD.saveAsObjectFile(savePath);
     }
+
+//    public static void main(String[] args) {
+//        List<Integer> thing = asList(1, 2, 3);
+//
+//        System.out.println(StreamSupport.stream(thing.spliterator(), false).count());
+//    }
 
     public static SolrInputDocument convertToSolrInputDocument(Document doc) {
         DocumentObjectBinder binder = new DocumentObjectBinder();
