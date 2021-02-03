1e404fd85983894176c90c033e93c76578dff268
lgonzales <lgonzales@ebi.ac.uk>
2021-02-03 12:28:26 +0000

    code review

--- uniprot-config/src/test/java/org/uniprot/store/config/searchfield/factory/SearchFieldConfigFactoryTest.java
+++ uniprot-config/src/test/java/org/uniprot/store/config/searchfield/factory/SearchFieldConfigFactoryTest.java
@@ -44,7 +44,7 @@ class SearchFieldConfigFactoryTest {
                 Arguments.of(UniProtDataType.SUGGEST, 3),
                 Arguments.of(UniProtDataType.TAXONOMY, 15),
                 Arguments.of(UniProtDataType.UNIPARC, 16),
-                 Arguments.of(UniProtDataType.UNIPROTKB, 434 + uniProtKBDBTypesCount),
+                Arguments.of(UniProtDataType.UNIPROTKB, 438 + uniProtKBDBTypesCount),
                 Arguments.of(UniProtDataType.UNIREF, 17),
                 Arguments.of(UniProtDataType.UNIRULE, 10));
     }
