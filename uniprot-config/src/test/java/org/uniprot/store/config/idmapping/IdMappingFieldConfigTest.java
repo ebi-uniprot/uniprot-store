d45c1a43567ad957bd68df39936ea7f77686bb56
eddturner <eddturner@gmai.com>
2021-03-02 21:19:25 +0000

    convert "uuw" from/to db names, to PIR's db names.

--- uniprot-config/src/test/java/org/uniprot/store/config/idmapping/IdMappingFieldConfigTest.java
+++ uniprot-config/src/test/java/org/uniprot/store/config/idmapping/IdMappingFieldConfigTest.java
@@ -1,25 +1,31 @@
 package org.uniprot.store.config.idmapping;
 
-import java.util.List;
-import java.util.Set;
-import java.util.stream.Collectors;
-
+import org.hamcrest.CoreMatchers;
+import org.hamcrest.MatcherAssert;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.uniprot.core.cv.xdb.UniProtDatabaseDetail;
 
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+import static org.uniprot.store.config.idmapping.IdMappingFieldConfig.*;
+
 /**
+ * Created 26/02/2021
+ *
  * @author sahmad
- * @created 26/02/2021
  */
 class IdMappingFieldConfigTest {
     @Test
     void testGetAllIdMappingFields() {
         List<UniProtDatabaseDetail> idMappingFields = IdMappingFieldConfig.getAllIdMappingTypes();
         Assertions.assertNotNull(idMappingFields);
-        Assertions.assertEquals(95, idMappingFields.size());
-        idMappingFields.stream()
-                .forEach(field -> Assertions.assertNotNull(field.getIdMappingName()));
+        // TODO: 02/03/2021 i'm not sure if we should test exact size... because
+        // drlineconfiguration.json is subject to change
+        Assertions.assertEquals(98, idMappingFields.size());
+        idMappingFields.forEach(field -> Assertions.assertNotNull(field.getIdMappingName()));
         // verify few mapped fields
         Set<String> names =
                 idMappingFields.stream()
@@ -28,5 +34,24 @@ class IdMappingFieldConfigTest {
         Assertions.assertTrue(names.contains("ACC"));
         Assertions.assertTrue(names.contains("NF100"));
         Assertions.assertTrue(names.contains("EMBL"));
+    }
+
+    @Test
+    void canMapDbNameToPIRDbName() {
+        MatcherAssert.assertThat(
+                IdMappingFieldConfig.convertDbNameToPIRDbName("UniProtKB Accession"),
+                CoreMatchers.is(ACC_STR));
+
+        MatcherAssert.assertThat(
+                IdMappingFieldConfig.convertDbNameToPIRDbName("UniProtKB AC/ID"),
+                CoreMatchers.is(ACC_ID_STR));
+
+        MatcherAssert.assertThat(
+                IdMappingFieldConfig.convertDbNameToPIRDbName("Gene Name"),
+                CoreMatchers.is(GENENAME_STR));
+
+        MatcherAssert.assertThat(
+                IdMappingFieldConfig.convertDbNameToPIRDbName("GenBank"),
+                CoreMatchers.is("EMBL_ID"));
     }
 }
