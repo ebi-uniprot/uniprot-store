26e628a12d1524b02d3eae7eaa37c7962c7fa43e
eddturner <eddturner@gmai.com>
2021-03-03 11:26:17 +0000

    change from using PIR's db names to our ones

--- uniprot-config/src/main/java/org/uniprot/store/config/idmapping/IdMappingFieldConfig.java
+++ uniprot-config/src/main/java/org/uniprot/store/config/idmapping/IdMappingFieldConfig.java
@@ -16,16 +16,27 @@ import java.util.stream.Collectors;
  * @created 26/02/2021
  */
 public class IdMappingFieldConfig {
+    public static final String ACC_STR = convertDisplayNameToName("UniProtKB");
+    public static final String ACC_ID_STR = convertDisplayNameToName("UniProtKB AC/ID");
+    public static final String SWISSPROT_STR = convertDisplayNameToName("UniProtKB/Swiss-Prot");
+    public static final String UPARC_STR = convertDisplayNameToName("UniParc");
+    public static final String UNIREF50_STR = convertDisplayNameToName("UniRef50");
+    public static final String UNIREF90_STR = convertDisplayNameToName("UniRef90");
+    public static final String UNIREF100_STR = convertDisplayNameToName("UniRef100");
+    public static final String GENENAME_STR = convertDisplayNameToName("Gene Name");
+
+    private static final String PIR_CRC64 = "CRC64";
+    public static final String PIR_ACC_STR = "ACC";
+    public static final String PIR_ACC_ID_STR = "ACC,ID";
+    public static final String PIR_SWISSPROT_STR = "SWISSPROT";
+    public static final String PIR_UPARC_STR = "UPARC";
+    public static final String PIR_UNIREF50_STR = "NF50";
+    public static final String PIR_UNIREF90_STR = "NF90";
+    public static final String PIR_UNIREF100_STR = "NF100";
+    public static final String PIR_GENENAME_STR = "GENENAME";
+
     private static final UniProtDatabaseTypes ALL_DB_TYPES = UniProtDatabaseTypes.INSTANCE;
     private static final String CRC64 = "CRC64";
-    public static final String ACC_STR = "ACC";
-    public static final String ACC_ID_STR = "ACC,ID";
-    public static final String SWISSPROT_STR = "SWISSPROT";
-    public static final String UPARC_STR = "UPARC";
-    public static final String UNIREF50_STR = "NF50";
-    public static final String UNIREF90_STR = "NF90";
-    public static final String UNIREF100_STR = "NF100";
-    public static final String GENENAME_STR = "GENENAME";
     private static List<UniProtDatabaseDetail> idMappingTypes = new ArrayList<>();
     private static Map<String, String> uniProtToPIRDbNameMap = new HashMap<>();
 
@@ -40,6 +51,23 @@ public class IdMappingFieldConfig {
             idMappingTypes.addAll(createUniProtCategoryIdMappingTypes());
             // add other missing types
             idMappingTypes.addAll(createMissingIdMappingTypes());
+
+            // replace special chars in name
+            idMappingTypes =
+                    idMappingTypes.stream()
+                            .map(
+                                    detail ->
+                                            new UniProtDatabaseDetail(
+                                                    convertDisplayNameToName(
+                                                            detail.getDisplayName()),
+                                                    detail.getDisplayName(),
+                                                    detail.getCategory(),
+                                                    detail.getUriLink(),
+                                                    detail.getAttributes(),
+                                                    detail.isImplicit(),
+                                                    detail.getLinkedReason(),
+                                                    detail.getIdMappingName()))
+                            .collect(Collectors.toList());
         }
 
         return idMappingTypes;
@@ -58,18 +86,17 @@ public class IdMappingFieldConfig {
         return uniProtToPIRDbNameMap.get(database);
     }
 
+    public static String convertDisplayNameToName(String value) {
+        value = value.replaceAll("[/,()]", "-");
+        value = value.replace(" ", "_");
+        return value;
+    }
+
     private static List<UniProtDatabaseDetail> createUniProtCategoryIdMappingTypes() {
         UniProtDatabaseCategory category = UniProtDatabaseCategory.UNKNOWN;
         UniProtDatabaseDetail uniProtKBAcc =
                 new UniProtDatabaseDetail(
-                        "UniProtKB Accession",
-                        "UniProtKB Accession",
-                        category,
-                        null,
-                        null,
-                        false,
-                        null,
-                        ACC_STR);
+                        "UniProtKB", "UniProtKB", category, null, null, false, null, PIR_ACC_STR);
         UniProtDatabaseDetail uniProtKBAccId =
                 new UniProtDatabaseDetail(
                         "UniProtKB AC/ID",
@@ -79,36 +106,36 @@ public class IdMappingFieldConfig {
                         null,
                         false,
                         null,
-                        ACC_ID_STR);
+                        PIR_ACC_ID_STR);
 
         UniProtDatabaseDetail uniProtSwissProtKBId =
                 new UniProtDatabaseDetail(
-                        "UniProtKB/SwissProt ACC",
-                        "UniProtKB/SwissProt ACC",
+                        "UniProtKB/Swiss-Prot",
+                        "UniProtKB/Swiss-Prot",
                         category,
                         null,
                         null,
                         false,
                         null,
-                        SWISSPROT_STR);
+                        PIR_SWISSPROT_STR);
 
         UniProtDatabaseDetail uniParc =
                 new UniProtDatabaseDetail(
-                        "UniParc", "UniParc", category, null, null, false, null, UPARC_STR);
+                        "UniParc", "UniParc", category, null, null, false, null, PIR_UPARC_STR);
         UniProtDatabaseDetail uniRef50 =
                 new UniProtDatabaseDetail(
-                        "UniRef50", "UniRef50", category, null, null, false, null, UNIREF50_STR);
+                        "UniRef50", "UniRef50", category, null, null, false, null, PIR_UNIREF50_STR);
         UniProtDatabaseDetail uniRef90 =
                 new UniProtDatabaseDetail(
-                        "UniRef90", "UniRef90", category, null, null, false, null, UNIREF90_STR);
+                        "UniRef90", "UniRef90", category, null, null, false, null, PIR_UNIREF90_STR);
         UniProtDatabaseDetail uniRef100 =
                 new UniProtDatabaseDetail(
-                        "UniRef100", "UniRef100", category, null, null, false, null, UNIREF100_STR);
+                        "UniRef100", "UniRef100", category, null, null, false, null, PIR_UNIREF100_STR);
         UniProtDatabaseDetail geneName =
                 new UniProtDatabaseDetail(
-                        "Gene Name", "Gene Name", category, null, null, false, null, GENENAME_STR);
+                        "Gene Name", "Gene Name", category, null, null, false, null, PIR_GENENAME_STR);
         UniProtDatabaseDetail crc64 =
-                new UniProtDatabaseDetail(CRC64, CRC64, category, null, null, false, null, CRC64);
+                new UniProtDatabaseDetail(CRC64, CRC64, category, null, null, false, null, PIR_CRC64);
         return List.of(
                 uniProtKBAcc,
                 uniProtKBAccId,
@@ -234,6 +261,4 @@ public class IdMappingFieldConfig {
     private static boolean hasIdMappingName(UniProtDatabaseDetail type) {
         return Utils.notNullNotEmpty(type.getIdMappingName());
     }
-
-    private IdMappingFieldConfig() {}
 }
