6726d107011e5e07dca0dd20f067ef225fa14aee
lgonzales <lgonzales@ebi.ac.uk>
2021-03-25 08:42:25 +0000

    log hash input

--- spark-indexer/src/main/java/org/uniprot/store/spark/indexer/main/verifiers/VerifyCitationData.java
+++ spark-indexer/src/main/java/org/uniprot/store/spark/indexer/main/verifiers/VerifyCitationData.java
@@ -10,6 +10,7 @@ import org.apache.spark.api.java.JavaSparkContext;
 import org.apache.spark.api.java.function.Function;
 import org.uniprot.core.citation.Citation;
 import org.uniprot.core.citation.CitationDatabase;
+import org.uniprot.core.citation.impl.*;
 import org.uniprot.core.uniprotkb.UniProtKBReference;
 import org.uniprot.core.util.Pair;
 import org.uniprot.core.util.PairImpl;
@@ -41,6 +42,7 @@ public class VerifyCitationData {
 
         List<Pair<Citation, Citation>> result = solrInputDocumentRDD
                 .flatMapToPair(new LiteratureUniProtKBReferencesMapper())
+                .filter(ref -> !hasPubmedId(ref._2))
                 .mapValues(ref -> ref.getCitation())
                 .repartition(30000)
                 .groupByKey()
@@ -49,11 +51,47 @@ public class VerifyCitationData {
                 .filter(tuple2 -> tuple2._2 != null && tuple2._2.getValue() != null)
                 .values()
                 .collect();
-
+        log.info("Duplicated Hash: {}", result.size());
         result.forEach(pair -> {
             log.info("----------------------");
             log.info("key: {}", pair.getKey());
             log.info("val: {}", pair.getValue());
+            switch (pair.getKey().getCitationType()){
+                case BOOK:
+                    BookImpl book = (BookImpl) pair.getKey();
+                    log.info("kha: {}", book.getHashInput());
+                    BookImpl boov = (BookImpl) pair.getValue();
+                    log.info("vha: {}", boov.getHashInput());
+                    break;
+                case PATENT:
+                    PatentImpl pa = (PatentImpl) pair.getKey();
+                    log.info("kha: {}", pa.getHashInput());
+                    PatentImpl pav = (PatentImpl) pair.getValue();
+                    log.info("vha: {}", pav.getHashInput());
+                    break;
+                case THESIS:
+                    ThesisImpl th = (ThesisImpl) pair.getKey();
+                    log.info("kha: {}", th.getHashInput());
+                    ThesisImpl thv = (ThesisImpl) pair.getValue();
+                    log.info("vha: {}", thv.getHashInput());
+                    break;
+                case SUBMISSION:
+                    SubmissionImpl sub = (SubmissionImpl) pair.getKey();
+                    log.info("kha: {}", sub.getHashInput());
+                    SubmissionImpl subv = (SubmissionImpl) pair.getValue();
+                    log.info("vha: {}", subv.getHashInput());
+                    break;
+                case ELECTRONIC_ARTICLE:
+                    ElectronicArticleImpl ea = (ElectronicArticleImpl) pair.getKey();
+                    log.info("kha: {}", ea.getHashInput());
+                    ElectronicArticleImpl eav = (ElectronicArticleImpl) pair.getValue();
+                    log.info("vha: {}", eav.getHashInput());
+                    break;
+                default:
+                    log.info("No print");
+            }
+
+
         });
         log.info("The End");
 
