0fe0ac79bbafafeccfc603c43f6416a599b9743e
Shadab Ahmad <sahmad@ebi.ac.uk>
2021-03-30 12:56:40 +0100

    add tests

--- uniprot-config/src/test/java/org/uniprot/store/config/returnfield/config/impl/UniParcCrossRefReturnFieldConfigImplIT.java
+++ uniprot-config/src/test/java/org/uniprot/store/config/returnfield/config/impl/UniParcCrossRefReturnFieldConfigImplIT.java
@@ -1,37 +1,37 @@
 package org.uniprot.store.config.returnfield.config.impl;
 
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import java.util.Collections;
+import java.util.Map;
+import java.util.stream.Stream;
+
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 import org.uniprot.core.json.parser.uniparc.UniParcEntryTest;
 import org.uniprot.core.parser.tsv.uniparc.UniParcEntryCrossRefValueMapper;
-import org.uniprot.core.parser.tsv.uniparc.UniParcEntryValueMapper;
 import org.uniprot.core.uniparc.UniParcEntry;
 import org.uniprot.store.config.UniProtDataType;
 import org.uniprot.store.config.returnfield.config.ReturnFieldConfig;
 import org.uniprot.store.config.returnfield.factory.ReturnFieldConfigFactory;
 import org.uniprot.store.config.returnfield.model.ReturnField;
 
-import java.util.Collections;
-import java.util.Map;
-import java.util.stream.Stream;
-
-import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
-
 /**
  * @author sahmad
  * @created 23/03/2021
  */
-public class UniParcCrossRefReturnFieldConfigImplIT {
+class UniParcCrossRefReturnFieldConfigImplIT {
 
     private static UniParcEntry entry;
     private static ReturnFieldConfig returnFieldConfig;
 
     @BeforeAll
     static void setUp() {
-        returnFieldConfig = ReturnFieldConfigFactory.getReturnFieldConfig(UniProtDataType.UNIPARC_CROSSREF);
+        returnFieldConfig =
+                ReturnFieldConfigFactory.getReturnFieldConfig(UniProtDataType.UNIPARC_CROSSREF);
         entry = UniParcEntryTest.getCompleteUniParcEntry();
     }
 
@@ -40,7 +40,9 @@ public class UniParcCrossRefReturnFieldConfigImplIT {
     void validReturnFieldWithMappedEntryDefined(String returnFieldName) {
         UniParcEntryCrossRefValueMapper entityValueMapper = new UniParcEntryCrossRefValueMapper();
         Map<String, String> mappedField =
-                entityValueMapper.mapEntity(entry.getUniParcCrossReferences().get(0), Collections.singletonList(returnFieldName));
+                entityValueMapper.mapEntity(
+                        entry.getUniParcCrossReferences().get(0),
+                        Collections.singletonList(returnFieldName));
         System.out.println(returnFieldName + " : " + mappedField.get(returnFieldName));
         assertNotNull(mappedField.get(returnFieldName));
         assertFalse(mappedField.get(returnFieldName).isEmpty());
