f35764402024a153b3c370f27f9da53acba46c1c
lgonzales <lgonzales@ebi.ac.uk>
2021-07-19 11:05:56 +0100

    create arba solr configuration

---
+++ uniprot-search/src/main/java/org/uniprot/store/search/document/arba/ArbaDocument.java
@@ -0,0 +1,64 @@
+package org.uniprot.store.search.document.arba;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.apache.solr.client.solrj.beans.Field;
+import org.uniprot.store.search.document.Document;
+
+import java.nio.ByteBuffer;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+/**
+ * @author lgonzales
+ * @since 16/07/2021
+ */
+@Builder(toBuilder = true)
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class ArbaDocument implements Document {
+
+    @Field("rule_id")
+    private String ruleId;
+
+    @Field("condition_value")
+    private Set<String> conditionValues = new HashSet<>();
+
+    @Field("feature_type")
+    private Set<String> featureTypes = new HashSet<>();
+
+    @Field("keyword")
+    private Set<String> keywords = new HashSet<>();
+
+    @Field("gene")
+    private Set<String> geneNames = new HashSet<>();
+
+    @Field("go")
+    private Set<String> goTerms = new HashSet<>();
+
+    @Field("protein_name")
+    private Set<String> proteinNames = new HashSet<>();
+
+    @Field("organism")
+    private Set<String> organismNames = new HashSet<>();
+
+    @Field("taxonomy")
+    private Set<String> taxonomyNames = new HashSet<>();
+
+    @Field("cc_*")
+    private Map<String, Set<String>> commentTypeValues = new HashMap<>();
+
+    @Field("rule_obj")
+    private ByteBuffer ruleObj;
+
+    @Override
+    public String getDocumentId() {
+        return ruleId;
+    }
+
+}
