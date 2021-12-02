3c87042d2c44ea6694e19ec40596de89a514661c
Shadab Ahmad <sahmad@ebi.ac.uk>
2021-12-02 12:06:50 +0000

    use GAF code to find eco for missing ids

--- spark-indexer/src/test/java/org/uniprot/store/spark/indexer/go/evidence/GOEvidencesFileMapperTest.java
+++ spark-indexer/src/test/java/org/uniprot/store/spark/indexer/go/evidence/GOEvidencesFileMapperTest.java
@@ -56,7 +56,8 @@ class GOEvidencesFileMapperTest {
 
     @Test
     void testGoLineWithMissingEcoId() throws Exception {
-        String goLine = "B0V2N1\tGO:0050804\tP\tmodulation of chemical synaptic transmission\tECO:0001225\tIMP\tPMID:22519304\tSynGO\tinvolved_in";
+        String goLine =
+                "B0V2N1\tGO:0050804\tP\tmodulation of chemical synaptic transmission\tECO:0001225\tIMP\tPMID:22519304\tSynGO\tinvolved_in";
         GOEvidencesFileMapper mapper = new GOEvidencesFileMapper();
         // when
         Tuple2<String, GOEvidence> tuple = mapper.call(goLine);
@@ -69,6 +70,15 @@ class GOEvidencesFileMapperTest {
 
         Evidence evidence = tuple._2.getEvidence();
         assertNotNull(evidence);
-        assertEquals(EvidenceCode.ECO_0000269, evidence.getEvidenceCode());
+        assertEquals(EvidenceCode.ECO_0000315, evidence.getEvidenceCode());
+    }
+
+    @Test
+    void testGoLineWithMissingGAFCodeInUniProt() throws Exception {
+        String goLine =
+                "B0V2N1\tGO:0050804\tP\tmodulation of chemical synaptic transmission\tECO:0001225\tBBC\tPMID:22519304\tSynGO\tinvolved_in";
+        GOEvidencesFileMapper mapper = new GOEvidencesFileMapper();
+
+        assertThrows(IllegalArgumentException.class, () -> mapper.call(goLine));
     }
 }
