96b9189e62fcc6c53b5e47ae20d090c178cb9e1d
LeonardoGonzales <lgonzales@ebi.ac.uk>
2021-12-07 09:33:04 +0000

    TRM-27118: add CHEBI inchikey value to cofactor and catalytic activity fields.

--- spark-indexer/src/test/java/org/uniprot/store/spark/indexer/proteome/mapper/ProteomeTaxonomyStatisticsMapperTest.java
+++ spark-indexer/src/test/java/org/uniprot/store/spark/indexer/proteome/mapper/ProteomeTaxonomyStatisticsMapperTest.java
@@ -1,5 +1,7 @@
 package org.uniprot.store.spark.indexer.proteome.mapper;
 
+import static org.junit.jupiter.api.Assertions.*;
+
 import org.junit.jupiter.api.Test;
 import org.uniprot.core.proteome.ProteomeEntry;
 import org.uniprot.core.proteome.ProteomeType;
@@ -7,20 +9,20 @@ import org.uniprot.core.proteome.impl.ProteomeEntryBuilder;
 import org.uniprot.core.taxonomy.TaxonomyStatistics;
 import org.uniprot.core.uniprotkb.taxonomy.impl.TaxonomyBuilder;
 import org.uniprot.store.spark.indexer.taxonomy.mapper.model.TaxonomyStatisticsWrapper;
-import scala.Tuple2;
 
-import static org.junit.jupiter.api.Assertions.*;
+import scala.Tuple2;
 
 class ProteomeTaxonomyStatisticsMapperTest {
 
     @Test
     void mapReferenceProteome() throws Exception {
         ProteomeTaxonomyStatisticsMapper mapper = new ProteomeTaxonomyStatisticsMapper();
-        ProteomeEntry entry = new ProteomeEntryBuilder()
-                .proteomeId("UP000000001")
-                .proteomeType(ProteomeType.REFERENCE)
-                .taxonomy(new TaxonomyBuilder().taxonId(100L).build())
-                .build();
+        ProteomeEntry entry =
+                new ProteomeEntryBuilder()
+                        .proteomeId("UP000000001")
+                        .proteomeType(ProteomeType.REFERENCE)
+                        .taxonomy(new TaxonomyBuilder().taxonId(100L).build())
+                        .build();
         Tuple2<String, TaxonomyStatisticsWrapper> result = mapper.call(entry);
         assertNotNull(result);
         assertEquals("100", result._1);
@@ -33,11 +35,12 @@ class ProteomeTaxonomyStatisticsMapperTest {
     @Test
     void mapNormalReferenceProteome() throws Exception {
         ProteomeTaxonomyStatisticsMapper mapper = new ProteomeTaxonomyStatisticsMapper();
-        ProteomeEntry entry = new ProteomeEntryBuilder()
-                .proteomeId("UP000000001")
-                .proteomeType(ProteomeType.NORMAL)
-                .taxonomy(new TaxonomyBuilder().taxonId(100L).build())
-                .build();
+        ProteomeEntry entry =
+                new ProteomeEntryBuilder()
+                        .proteomeId("UP000000001")
+                        .proteomeType(ProteomeType.NORMAL)
+                        .taxonomy(new TaxonomyBuilder().taxonId(100L).build())
+                        .build();
         Tuple2<String, TaxonomyStatisticsWrapper> result = mapper.call(entry);
         assertNotNull(result);
         assertEquals("100", result._1);
