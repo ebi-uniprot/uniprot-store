0a3b8787030cf8b4e769862afd1efe5b8a4b0328
LeonardoGonzales <lgonzales@ebi.ac.uk>
2022-06-24 22:28:39 +0100

    add logic to restrict uniparc entry size

--- spark-indexer/src/main/java/org/uniprot/store/spark/indexer/uniparc/mapper/UniParcEntryKeyMapper.java
+++ spark-indexer/src/main/java/org/uniprot/store/spark/indexer/uniparc/mapper/UniParcEntryKeyMapper.java
@@ -1,9 +1,13 @@
 package org.uniprot.store.spark.indexer.uniparc.mapper;
 
 import org.apache.spark.api.java.function.PairFunction;
+import org.uniprot.core.uniparc.UniParcCrossReference;
 import org.uniprot.core.uniparc.UniParcEntry;
 
+import org.uniprot.core.uniparc.impl.UniParcEntryBuilder;
 import scala.Tuple2;
+
+import java.util.List;
 
 /**
  * @author lgonzales
@@ -14,6 +18,11 @@ public class UniParcEntryKeyMapper implements PairFunction<UniParcEntry, String,
 
     @Override
     public Tuple2<String, UniParcEntry> call(UniParcEntry entry) throws Exception {
+        if (entry.getUniParcCrossReferences().size() > 10000){
+            UniParcEntryBuilder builder = UniParcEntryBuilder.from(entry);
+            builder.uniParcCrossReferencesSet(entry.getUniParcCrossReferences().subList(0, 10000));
+            entry = builder.build();
+        }
         return new Tuple2<>(entry.getUniParcId().getValue(), entry);
     }
 }
