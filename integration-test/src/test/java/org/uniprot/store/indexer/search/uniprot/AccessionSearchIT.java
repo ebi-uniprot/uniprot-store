0d90e921e749fa349ddd10f808ffcd09b540a4f0
Shadab Ahmad <sahmad@ebi.ac.uk>
2022-07-15 17:28:45 +0100

    support include isoform in idmapping

--- integration-test/src/test/java/org/uniprot/store/indexer/search/uniprot/AccessionSearchIT.java
+++ integration-test/src/test/java/org/uniprot/store/indexer/search/uniprot/AccessionSearchIT.java
@@ -1,7 +1,9 @@
 package org.uniprot.store.indexer.search.uniprot;
 
 import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.*;
+import static org.hamcrest.Matchers.contains;
+import static org.hamcrest.Matchers.containsInAnyOrder;
+import static org.hamcrest.Matchers.notNullValue;
 import static org.hamcrest.collection.IsEmptyCollection.empty;
 import static org.hamcrest.core.Is.is;
 import static org.uniprot.store.indexer.search.uniprot.TestUtils.convertToUniProtEntry;
@@ -77,15 +79,6 @@ class AccessionSearchIT {
     void upperCaseAccessionFromEntry3MatchesEntry3() { //  Query query =
         // UniProtQueryBuilder.accession(PRIMARY_ACCESSION3);
         String query = "accession:" + PRIMARY_ACCESSION3;
-        QueryResponse response = searchEngine.getQueryResponse(query);
-
-        List<String> retrievedAccessions = searchEngine.getIdentifiers(response);
-        assertThat(retrievedAccessions, contains(PRIMARY_ACCESSION3));
-    }
-
-    @Test
-    void lowerCaseAccessionFromEntry3MatchesEntry3() {
-        String query = "accession:" + PRIMARY_ACCESSION3.toLowerCase();
         QueryResponse response = searchEngine.getQueryResponse(query);
 
         List<String> retrievedAccessions = searchEngine.getIdentifiers(response);
