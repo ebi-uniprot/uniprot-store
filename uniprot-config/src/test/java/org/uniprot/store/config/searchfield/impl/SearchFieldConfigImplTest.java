f58a74b66022bfc28e87b13e0a6cbefce8372170
LeonardoGonzales <lgonzales@ebi.ac.uk>
2023-02-23 08:17:44 +0000

    Add Any experimental evidence search field in advanced search menu

--- uniprot-config/src/test/java/org/uniprot/store/config/searchfield/impl/SearchFieldConfigImplTest.java
+++ uniprot-config/src/test/java/org/uniprot/store/config/searchfield/impl/SearchFieldConfigImplTest.java
@@ -32,7 +32,7 @@ class SearchFieldConfigImplTest {
     void testSize() {
         List<SearchFieldItem> items = searchFieldConfig.getSearchFieldItems();
         Assertions.assertNotNull(items);
-        assertEquals(467, items.size());
+        assertEquals(468, items.size());
     }
 
     @Test
@@ -62,7 +62,7 @@ class SearchFieldConfigImplTest {
                                         StringUtils.isNotEmpty(i.getFieldName())
                                                 && !(i.getFieldName().startsWith("xref_count_")))
                         .count();
-        Assertions.assertEquals((451 - 176), xrefCountFieldsCount);
+        Assertions.assertEquals((452 - 176), xrefCountFieldsCount);
     }
 
     @Test
