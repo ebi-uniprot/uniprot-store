9d0ed04f58e0b6c3616fe53bc4d28f9d2a199814
ktibrahim <ibrahim@ebi.ac.uk>
2023-03-07 14:40:12 +0000

    removed empty lines

--- integration-test/src/main/java/org/uniprot/store/indexer/uniprot/mockers/UniProtEntryMocker.java
+++ integration-test/src/main/java/org/uniprot/store/indexer/uniprot/mockers/UniProtEntryMocker.java
@@ -33,9 +33,7 @@ public class UniProtEntryMocker {
         SP_ISOFORM("P21802-2.dat"),
         SP_CANONICAL_ISOFORM("P21802-1.dat"),
         WITH_DEMERGED_SEC_ACCESSION("P63150.dat"),
-
         ACC("B3GAT1.dat"),
-
         ACCANDGENE("Q9P2W7.dat");
 
         private final String fileName;
