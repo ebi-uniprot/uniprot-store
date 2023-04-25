166e09c0f774a60a448eef18e3f29a2250f54e7b
LeonardoGonzales <lgonzales@ebi.ac.uk>
2023-04-25 13:02:45 +0100

    clean ccev, ftev and ftlen index data

--- spark-indexer/src/main/java/org/uniprot/store/spark/indexer/uniprot/converter/UniProtEntryCommentsConverter.java
+++ spark-indexer/src/main/java/org/uniprot/store/spark/indexer/uniprot/converter/UniProtEntryCommentsConverter.java
@@ -68,8 +68,6 @@ class UniProtEntryCommentsConverter implements Serializable {
             String commentVal = fbuilder.buildString(comment);
             commentValues.add(commentVal);
             document.content.add(commentVal);
-            Collection<String> evValues =
-                    document.commentEvMap.computeIfAbsent(evField, k -> new HashSet<>());
             Set<String> evidences = fetchEvidences(comment);
 
             if (canAddExperimental(
@@ -85,7 +83,6 @@ class UniProtEntryCommentsConverter implements Serializable {
                 evidences.add(EvidenceCode.Category.EXPERIMENTAL.name().toLowerCase());
                 document.evidenceExperimental = true;
             }
-            evValues.addAll(evidences);
 
             ProteinsWith.from(comment.getCommentType())
                     .map(ProteinsWith::getValue)
@@ -181,14 +178,12 @@ class UniProtEntryCommentsConverter implements Serializable {
             // '*'
         }
         document.ap.addAll(values);
-        document.apEv.addAll(evidences);
         if (canAddExperimental(
                 commentType.isAddExperimental(), commentVal, document.reviewed, evidences)) {
             Collection<String> apExp =
                     document.commentMap.computeIfAbsent(CC_AP_EXPERIMENTAL, k -> new HashSet<>());
             apExp.addAll(values);
             apExp.addAll(events);
-            document.apEv.add(EvidenceCode.Category.EXPERIMENTAL.name().toLowerCase());
             document.evidenceExperimental = true;
         }
         for (String event : events) {
@@ -196,25 +191,21 @@ class UniProtEntryCommentsConverter implements Serializable {
                 document.apApu.addAll(values);
                 addExperimentalEvidence(
                         commentType, document, values, evidences, CC_AP_APU_EXPERIMENTAL);
-                document.apApuEv.addAll(evidences);
             }
             if ("alternative splicing".equalsIgnoreCase(event)) {
                 document.apAs.addAll(values);
                 addExperimentalEvidence(
                         commentType, document, values, evidences, CC_AP_AS_EXPERIMENTAL);
-                document.apAsEv.addAll(evidences);
             }
             if ("alternative initiation".equalsIgnoreCase(event)) {
                 document.apAi.addAll(values);
                 addExperimentalEvidence(
                         commentType, document, values, evidences, CC_AP_AI_EXPERIMENTAL);
-                document.apAiEv.addAll(evidences);
             }
             if ("ribosomal frameshifting".equalsIgnoreCase(event)) {
                 document.apRf.addAll(values);
                 addExperimentalEvidence(
                         commentType, document, values, evidences, CC_AP_RF_EXPERIMENTAL);
-                document.apRfEv.addAll(evidences);
             }
         }
     }
@@ -253,7 +244,6 @@ class UniProtEntryCommentsConverter implements Serializable {
                     noteValues,
                     textEvidences,
                     CC_COFACTOR_NOTE_EXPERIMENTAL);
-            document.cofactorNoteEv.addAll(textEvidences);
         }
     }
 
@@ -273,10 +263,8 @@ class UniProtEntryCommentsConverter implements Serializable {
             document.commentMap
                     .computeIfAbsent(CC_COFACTOR_CHEBI_EXPERIMENTAL, k -> new HashSet<>())
                     .addAll(cofactorValues);
-            document.cofactorChebiEv.add(EvidenceCode.Category.EXPERIMENTAL.name().toLowerCase());
             document.evidenceExperimental = true;
         }
-        document.cofactorChebiEv.addAll(evidences);
     }
 
     private void convertCommentSC(SequenceCautionComment comment, UniProtDocument document) {
@@ -294,7 +282,6 @@ class UniProtEntryCommentsConverter implements Serializable {
         Set<String> evidence = UniProtEntryConverterUtil.extractEvidence(comment.getEvidences());
         CommentType commentType = CommentType.SEQUENCE_CAUTION;
         addExperimentalEvidence(commentType, document, cautionValues, evidence, CC_SC_EXPERIMENTAL);
-        document.seqCautionEv.addAll(evidence);
         switch (comment.getSequenceCautionType()) {
             case FRAMESHIFT:
                 document.seqCautionFrameshift.add(val);
@@ -315,7 +302,6 @@ class UniProtEntryCommentsConverter implements Serializable {
                 document.seqCautionMisc.add(val);
                 addExperimentalEvidence(
                         commentType, document, Set.of(val), evidence, CC_SC_MISC_EXPERIMENTAL);
-                document.seqCautionMiscEv.addAll(evidence);
                 break;
             default:
         }
@@ -383,8 +369,6 @@ class UniProtEntryCommentsConverter implements Serializable {
                     EvidenceCode.Category.EXPERIMENTAL.name().toLowerCase());
             document.evidenceExperimental = true;
         }
-        document.bpcpEv.addAll(temperatureDependenceEvidences);
-        document.bpcpTempDependenceEv.addAll(temperatureDependenceEvidences);
     }
 
     private void convertRedoxPotential(UniProtDocument document, RedoxPotential redoxPotential) {
@@ -407,8 +391,6 @@ class UniProtEntryCommentsConverter implements Serializable {
             redoxPotentialEvidences.add(EvidenceCode.Category.EXPERIMENTAL.name().toLowerCase());
             document.evidenceExperimental = true;
         }
-        document.bpcpEv.addAll(redoxPotentialEvidences);
-        document.bpcpRedoxPotentialEv.addAll(redoxPotentialEvidences);
     }
 
     private void convertPhDependence(UniProtDocument document, PhDependence phDependence) {
@@ -431,8 +413,6 @@ class UniProtEntryCommentsConverter implements Serializable {
             phDependenceEvidences.add(EvidenceCode.Category.EXPERIMENTAL.name().toLowerCase());
             document.evidenceExperimental = true;
         }
-        document.bpcpPhDependenceEv.addAll(phDependenceEvidences);
-        document.bpcpEv.addAll(phDependenceEvidences);
     }
 
     private void convertAbsorption(UniProtDocument document, Absorption absorption) {
@@ -462,8 +442,6 @@ class UniProtEntryCommentsConverter implements Serializable {
             absorptionEvidences.add(EvidenceCode.Category.EXPERIMENTAL.name().toLowerCase());
             document.evidenceExperimental = true;
         }
-        document.bpcpAbsorptionEv.addAll(absorptionEvidences);
-        document.bpcpEv.addAll(absorptionEvidences);
     }
 
     private void convertKineticParameters(UniProtDocument document, KineticParameters kp) {
@@ -522,8 +500,6 @@ class UniProtEntryCommentsConverter implements Serializable {
             kineticEvidenceValues.add(EvidenceCode.Category.EXPERIMENTAL.name().toLowerCase());
             document.evidenceExperimental = true;
         }
-        document.bpcpKineticsEv.addAll(kineticEvidenceValues);
-        document.bpcpEv.addAll(kineticEvidenceValues);
     }
 
     private List<String> getTextsValue(List<EvidencedValue> texts) {
@@ -581,7 +557,6 @@ class UniProtEntryCommentsConverter implements Serializable {
                     noteValues,
                     noteEv,
                     CC_SCL_NOTE_EXPERIMENTAL);
-            document.subcellLocationNoteEv.addAll(noteEv);
         }
     }
 
@@ -597,7 +572,6 @@ class UniProtEntryCommentsConverter implements Serializable {
                 locationTerm,
                 locationEv,
                 CC_SCL_TERM_EXPERIMENTAL);
-        document.subcellLocationTermEv.addAll(locationEv);
     }
 
     private void updateFamily(String val, UniProtDocument document) {
--- spark-indexer/src/main/java/org/uniprot/store/spark/indexer/uniprot/converter/UniProtEntryFeatureConverter.java
+++ spark-indexer/src/main/java/org/uniprot/store/spark/indexer/uniprot/converter/UniProtEntryFeatureConverter.java
@@ -8,7 +8,6 @@ import java.util.List;
 import java.util.Set;
 
 import org.uniprot.core.CrossReference;
-import org.uniprot.core.uniprotkb.evidence.EvidenceCode;
 import org.uniprot.core.uniprotkb.feature.*;
 import org.uniprot.core.util.Utils;
 import org.uniprot.store.search.document.uniprot.ProteinsWith;
@@ -67,18 +66,7 @@ class UniProtEntryFeatureConverter {
             }
 
             // start and end of location
-            int length =
-                    feature.getLocation().getEnd().getValue()
-                            - feature.getLocation().getStart().getValue()
-                            + 1;
             Set<String> evidences = extractEvidence(feature.getEvidences());
-            Collection<Integer> lengthList =
-                    document.featureLengthMap.computeIfAbsent(lengthField, k -> new HashSet<>());
-            lengthList.add(length);
-
-            Collection<String> evidenceList =
-                    document.featureEvidenceMap.computeIfAbsent(evField, k -> new HashSet<>());
-            evidenceList.addAll(evidences);
 
             document.featuresMap
                     .computeIfAbsent(field, k -> new HashSet<>())
@@ -95,7 +83,6 @@ class UniProtEntryFeatureConverter {
                 document.featuresMap
                         .computeIfAbsent(experimentalField, k -> new HashSet<>())
                         .addAll(featureValueList);
-                evidenceList.add(EvidenceCode.Category.EXPERIMENTAL.name().toLowerCase());
                 document.evidenceExperimental = true;
             }
         }
