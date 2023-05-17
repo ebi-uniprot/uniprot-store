8cbb2e42cc67d98c2fdf034e969cfb2d4be4905a
Shadab Ahmad <sahmad@ebi.ac.uk>
2023-05-17 09:07:51 +0100

    rename application.prop

--- spark-indexer/src/main/java/org/uniprot/store/spark/indexer/common/util/SparkUtils.java
+++ spark-indexer/src/main/java/org/uniprot/store/spark/indexer/common/util/SparkUtils.java
@@ -91,7 +91,15 @@ public class SparkUtils {
                         .getLocation();
         System.out.println("resourceURL is " + resourceURL.toString());
         System.out.println("Locale.getDefault() " + Locale.getDefault());
-        return ResourceBundle.getBundle(baseName, Locale.getDefault());
+        try (URLClassLoader urlLoader = new URLClassLoader(new java.net.URL[] {resourceURL})) {
+            System.out.println("Inside try block");
+            // try to load from the directory that the application is being executed
+            return ResourceBundle.getBundle(baseName, Locale.getDefault(), urlLoader);
+        } catch (MissingResourceException | IOException e) {
+            // load from the classpath
+            System.out.println("Inside catch block");
+            return ResourceBundle.getBundle(baseName);
+        }
     }
 
     public static JavaSparkContext loadSparkContext(ResourceBundle applicationConfig) {
