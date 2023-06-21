f0cc8cc9c646bd05877744bd1f3567639c0e36bc
Shadab Ahmad <sahmad@ebi.ac.uk>
2023-06-21 18:12:07 +0100

    remove use parsefile

--- spark-indexer/src/main/java/org/uniprot/store/spark/indexer/common/util/SparkUtils.java
+++ spark-indexer/src/main/java/org/uniprot/store/spark/indexer/common/util/SparkUtils.java
@@ -84,7 +84,7 @@ public class SparkUtils {
     public static Config loadApplicationProperty(String baseName) {
         try {
             log.info("******** starting to read file ********");
-            Config config = ConfigFactory.load();
+            Config config = ConfigFactory.load(baseName);
 //            Config config = ConfigFactory.parseFile(new File("/homes/uni_adm/slurm-test/indexer/conf/application.properties"))
 //                    .withFallback(baseConfig);
             config.entrySet()
