b9a2bf9170b9f0a8caf34ee8884797e81bebfeb4
LeonardoGonzales <lgonzales@ebi.ac.uk>
2023-07-18 22:08:48 +0100

    add organism

--- spark-indexer/src/main/java/org/uniprot/store/spark/indexer/main/experimental/ListBigUniParcs.java
+++ spark-indexer/src/main/java/org/uniprot/store/spark/indexer/main/experimental/ListBigUniParcs.java
@@ -2,13 +2,21 @@ package org.uniprot.store.spark.indexer.main.experimental;
 
 import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
-import org.apache.spark.sql.SparkSession;
+import org.uniprot.core.uniparc.UniParcCrossReference;
 import org.uniprot.core.uniparc.UniParcEntry;
+import org.uniprot.core.uniprotkb.taxonomy.Organism;
 import org.uniprot.store.spark.indexer.common.JobParameter;
 import org.uniprot.store.spark.indexer.common.util.SparkUtils;
 import org.uniprot.store.spark.indexer.uniparc.UniParcRDDTupleReader;
 
 import com.typesafe.config.Config;
+
+import java.util.Comparator;
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 
 public class ListBigUniParcs {
 
@@ -34,10 +42,22 @@ public class ListBigUniParcs {
                             .build();
             UniParcRDDTupleReader reader = new UniParcRDDTupleReader(jobParameter, true);
             JavaRDD<UniParcEntry> uniParcRDD = reader.load();
-            uniParcRDD.filter(entry -> entry.getUniParcCrossReferences().size() >= 10000)
-                    .map(entry -> entry.getUniParcId().getValue() + "\t" + entry.getUniParcCrossReferences().size())
+            uniParcRDD.filter(entry -> entry.getUniParcCrossReferences().size() >= 1)
+                    .map(entry -> entry.getUniParcId().getValue() + "\t" + getOrganism(entry.getUniParcCrossReferences()) + "\t" + entry.getUniParcCrossReferences().size())
                     .repartition(150)
                     .saveAsTextFile(outputFilePath);
         }
+    }
+
+    private static String getOrganism(List<UniParcCrossReference> uniParcCrossReferences) {
+        return "" + uniParcCrossReferences.stream()
+                .map(UniParcCrossReference::getOrganism)
+                .filter(Objects::nonNull)
+                .map(Organism::getTaxonId)
+                .collect(Collectors.groupingBy(Function.identity(), Collectors.counting()))
+                .entrySet().stream()
+                .sorted(Map.Entry.comparingByValue(Comparator.reverseOrder()))
+                .map(Map.Entry::getKey)
+                .findFirst().orElse(0L);
     }
 }
