91ca191f5f633544ff1c937331b86cef85c1677a
supun <supun@ebi.ac.uk>
2023-08-04 14:20:12 +0100

    format

--- uniprot-search/src/test/java/org/uniprot/store/search/field/validator/FieldRegexConstantsTest.java
+++ uniprot-search/src/test/java/org/uniprot/store/search/field/validator/FieldRegexConstantsTest.java
@@ -1,9 +1,9 @@
 package org.uniprot.store.search.field.validator;
 
-import org.junit.jupiter.api.Test;
-
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
 
 class FieldRegexConstantsTest {
 
