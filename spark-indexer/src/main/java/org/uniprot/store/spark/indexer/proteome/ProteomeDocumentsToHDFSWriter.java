8c347a636408ef772cfb5c1043a1470c500b6ad3
supun <supun@ebi.ac.uk>
2023-08-14 16:06:31 +0100

    wip

--- spark-indexer/src/main/java/org/uniprot/store/spark/indexer/proteome/ProteomeDocumentsToHDFSWriter.java
+++ spark-indexer/src/main/java/org/uniprot/store/spark/indexer/proteome/ProteomeDocumentsToHDFSWriter.java
@@ -1,15 +1,24 @@
 package org.uniprot.store.spark.indexer.proteome;
 
 import org.apache.spark.api.java.JavaPairRDD;
+import org.apache.spark.api.java.JavaRDD;
+import org.apache.spark.api.java.Optional;
+import org.apache.spark.storage.StorageLevel;
 import org.uniprot.core.proteome.ProteomeEntry;
 import org.uniprot.core.proteome.ProteomeStatistics;
+import org.uniprot.store.search.SolrCollection;
+import org.uniprot.store.search.document.proteome.ProteomeDocument;
 import org.uniprot.store.spark.indexer.common.JobParameter;
+import org.uniprot.store.spark.indexer.common.util.SolrUtils;
 import org.uniprot.store.spark.indexer.common.writer.DocumentsToHDFSWriter;
 import org.uniprot.store.spark.indexer.proteome.mapper.ProteomeStatisticsAggregationMapper;
 import org.uniprot.store.spark.indexer.uniprot.UniProtKBRDDTupleReader;
+import scala.Tuple2;
 
 import java.util.List;
 import java.util.ResourceBundle;
+
+import static org.uniprot.store.spark.indexer.common.util.SparkUtils.getCollectionOutputReleaseDirPath;
 
 public class ProteomeDocumentsToHDFSWriter implements DocumentsToHDFSWriter {
     private final JobParameter parameter;
@@ -25,22 +34,39 @@ public class ProteomeDocumentsToHDFSWriter implements DocumentsToHDFSWriter {
 
     @Override
     public void writeIndexDocumentsToHDFS() {
+        ProteomeRDDReader proteomeRDDReader = getProteomeRDDReader();
+        JavaPairRDD<String, ProteomeEntry> proteomeEntryJavaPairRDD = proteomeRDDReader.load().persist(StorageLevel.DISK_ONLY());
+        JavaRDD<Tuple2<ProteomeEntry, Optional<ProteomeStatistics>>> proteomeStatisticsRDD = getProteomeStatisticsRDD(proteomeEntryJavaPairRDD);
 
+        JavaRDD<ProteomeDocument> proteomeDocumentJavaRDD = proteomeEntryJavaPairRDD
+                .leftOuterJoin(proteomeStatisticsRDD)
+                .mapValues();
+
+        saveToHDFS(proteomeDocumentJavaRDD);
     }
 
-    private JavaPairRDD<String, ProteomeStatistics> getProteomeStatisticsRDD(JavaPairRDD<String, ProteomeEntry> taxonomyRDD) {
-        //statistics aggregation mapper init
-
+    private JavaRDD<Tuple2<ProteomeEntry, Optional<ProteomeStatistics>>> getProteomeStatisticsRDD(JavaPairRDD<String, ProteomeEntry> proteomeRDD) {
         UniProtKBRDDTupleReader uniProtKBReader =
                 new UniProtKBRDDTupleReader(this.parameter, false);
         ProteomeStatisticsAggregationMapper agg = new ProteomeStatisticsAggregationMapper();
 
-        JavaPairRDD<String, ProteomeStatistics> proteomeStats = uniProtKBReader.loadFlatFileToRDD()
+        JavaPairRDD<String, ProteomeStatistics> proteomeStatisticsRDD = uniProtKBReader.loadFlatFileToRDD()
                 .map(new ProteomeJoinMapper())
                 .flatMap(List::iterator)
                 .mapToPair(stringProteomeStatisticsTuple2 -> stringProteomeStatisticsTuple2)
                 .aggregateByKey(null, agg, agg);
 
-        return proteomeStats
+        return proteomeRDD
+                .leftOuterJoin(proteomeStatisticsRDD)
+                .values();
+    }
+    void saveToHDFS(JavaRDD<ProteomeDocument> proteomeDocumentJavaRDD) {
+        String hdfsPath =
+                getCollectionOutputReleaseDirPath(config, releaseName, SolrCollection.proteome);
+        SolrUtils.saveSolrInputDocumentRDD(proteomeDocumentJavaRDD, hdfsPath);
+    }
+
+    ProteomeRDDReader getProteomeRDDReader() {
+        return new ProteomeRDDReader(parameter, true);
     }
 }
