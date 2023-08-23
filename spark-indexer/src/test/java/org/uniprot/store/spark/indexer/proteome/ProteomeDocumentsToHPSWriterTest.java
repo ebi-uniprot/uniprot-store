6503ab71f2487be279ec3cc9fb849824d0cfc9df
supun <supun@ebi.ac.uk>
2023-08-23 09:51:31 +0100

    stat reader test

--- spark-indexer/src/test/java/org/uniprot/store/spark/indexer/proteome/ProteomeDocumentsToHPSWriterTest.java
+++ spark-indexer/src/test/java/org/uniprot/store/spark/indexer/proteome/ProteomeDocumentsToHPSWriterTest.java
@@ -26,27 +26,27 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.uniprot.store.spark.indexer.common.util.CommonVariables.SPARK_LOCAL_MASTER;
 
 class ProteomeDocumentsToHPSWriterTest {
-    public static final String PROTEOME_ID_0 = "proteomeId0";
-    public static final String PROTEOME_ID_1 = "proteomeId1";
-    public static final String PROTEOME_ID_2 = "proteomeId2";
-    public static final String[] PROTEOME_IDS = new String[]{PROTEOME_ID_0, PROTEOME_ID_1, PROTEOME_ID_2};
-    public static final int ORGANISM_ID_0 = 19;
-    public static final int ORGANISM_ID_1 = 333;
-    public static final int ORGANISM_ID_2 = 67;
-    public static final Integer[] ORGANISM_IDS = new Integer[]{ORGANISM_ID_0, ORGANISM_ID_1, ORGANISM_ID_2};
-    public static final long REVIEWED_PROTEIN_COUNT_0 = 49;
-    public static final long REVIEWED_PROTEIN_COUNT_1 = 53;
-    public static final Long[] REVIEWED_PROTEIN_COUNTS = new Long[]{REVIEWED_PROTEIN_COUNT_0, REVIEWED_PROTEIN_COUNT_1, 0L};
-    public static final long UNREVIEWED_PROTEIN_COUNT_0 = 5555;
-    public static final long UNREVIEWED_PROTEIN_COUNT_1 = 10;
-    public static final Long[] UNREVIEWED_PROTEIN_COUNTS = new Long[]{UNREVIEWED_PROTEIN_COUNT_0, UNREVIEWED_PROTEIN_COUNT_1, 0L};
-    public static final long ISOFORM_PROTEIN_COUNT_0 = 36;
-    public static final long ISOFORM_PROTEIN_COUNT_1 = 9998;
-    public static final Long[] ISOFORM_PROTEIN_COUNTS = new Long[]{ISOFORM_PROTEIN_COUNT_0, ISOFORM_PROTEIN_COUNT_1, 0L};
-    public static final String ORGANISM_SORT_0 = "organismSort0";
-    public static final String ORGANISM_SORT_1 = "organismSort1";
-    public static final String ORGANISM_SORT_2 = "organismSort2";
-    public static final String[] ORGANISM_SORTS = new String[]{ORGANISM_SORT_0, ORGANISM_SORT_1, ORGANISM_SORT_2};
+    private static final String PROTEOME_ID_0 = "proteomeId0";
+    private static final String PROTEOME_ID_1 = "proteomeId1";
+    private static final String PROTEOME_ID_2 = "proteomeId2";
+    private static final String[] PROTEOME_IDS = new String[]{PROTEOME_ID_0, PROTEOME_ID_1, PROTEOME_ID_2};
+    private static final int ORGANISM_ID_0 = 19;
+    private static final int ORGANISM_ID_1 = 333;
+    private static final int ORGANISM_ID_2 = 67;
+    private static final Integer[] ORGANISM_IDS = new Integer[]{ORGANISM_ID_0, ORGANISM_ID_1, ORGANISM_ID_2};
+    private static final long REVIEWED_PROTEIN_COUNT_0 = 49;
+    private static final long REVIEWED_PROTEIN_COUNT_1 = 53;
+    private static final Long[] REVIEWED_PROTEIN_COUNTS = new Long[]{REVIEWED_PROTEIN_COUNT_0, REVIEWED_PROTEIN_COUNT_1, 0L};
+    private static final long UNREVIEWED_PROTEIN_COUNT_0 = 5555;
+    private static final long UNREVIEWED_PROTEIN_COUNT_1 = 10;
+    private static final Long[] UNREVIEWED_PROTEIN_COUNTS = new Long[]{UNREVIEWED_PROTEIN_COUNT_0, UNREVIEWED_PROTEIN_COUNT_1, 0L};
+    private static final long ISOFORM_PROTEIN_COUNT_0 = 36;
+    private static final long ISOFORM_PROTEIN_COUNT_1 = 9998;
+    private static final Long[] ISOFORM_PROTEIN_COUNTS = new Long[]{ISOFORM_PROTEIN_COUNT_0, ISOFORM_PROTEIN_COUNT_1, 0L};
+    private static final String ORGANISM_SORT_0 = "organismSort0";
+    private static final String ORGANISM_SORT_1 = "organismSort1";
+    private static final String ORGANISM_SORT_2 = "organismSort2";
+    private static final String[] ORGANISM_SORTS = new String[]{ORGANISM_SORT_0, ORGANISM_SORT_1, ORGANISM_SORT_2};
     private static final List<String> ORGANISM_NAME_0 = List.of("on0");
     private static final List<String> ORGANISM_NAME_1 = List.of("on1");
     private static final List<String> ORGANISM_NAME_2 = List.of("on2");
@@ -59,9 +59,30 @@ class ProteomeDocumentsToHPSWriterTest {
     private static final List<Integer> LINEAGE_ID_1 = List.of(11);
     private static final List<Integer> LINEAGE_ID_2 = List.of(12);
     private static final List<Integer>[] LINEAGE_IDS = new List[]{LINEAGE_ID_0, LINEAGE_ID_1, LINEAGE_ID_2};
-    public static final String RELEASE_NAME = "23_03";
+    private static final String RELEASE_NAME = "23_03";
     private static JavaPairRDD<String, ProteomeEntry> proteomeRDD;
     private static JavaPairRDD<String, ProteomeStatistics> statisticsRDD;
+
+    @Test
+    void writeIndexDocumentsToHPS() {
+        Config application = SparkUtils.loadApplicationProperty();
+        try (JavaSparkContext sparkContext = SparkUtils.loadSparkContext(application, SPARK_LOCAL_MASTER)) {
+            JobParameter jobParameter = JobParameter.builder().sparkContext(sparkContext).applicationConfig(application).releaseName(RELEASE_NAME).build();
+            proteomeRDD = sparkContext.parallelizePairs(getProteomeTuples());
+            statisticsRDD = sparkContext.parallelizePairs(getStatisticsTuples());
+            ProteomeDocumentsToHPSWriterFake writer = new ProteomeDocumentsToHPSWriterFake(jobParameter);
+
+            writer.writeIndexDocumentsToHPS();
+
+            List<ProteomeDocument> savedDocuments = writer.savedDocuments;
+            assertEquals(3, savedDocuments.size());
+            int index = 0;
+            for (ProteomeDocument savedDocument : savedDocuments) {
+                assertResult(savedDocument, index);
+                index++;
+            }
+        }
+    }
 
     private static List<Tuple2<String, ProteomeStatistics>> getStatisticsTuples() {
         return List.of(
@@ -78,25 +99,6 @@ class ProteomeDocumentsToHPSWriterTest {
                 new Tuple2<>(PROTEOME_ID_1, new ProteomeEntryBuilder().proteomeId(PROTEOME_ID_1).taxonomy(new TaxonomyBuilder().taxonId(ORGANISM_ID_1).build()).build()),
                 new Tuple2<>(PROTEOME_ID_2, new ProteomeEntryBuilder().proteomeId(PROTEOME_ID_2).taxonomy(new TaxonomyBuilder().taxonId(ORGANISM_ID_2).build()).build())
         );
-    }
-
-    @Test
-    void writeIndexDocumentsToHPS() {
-        Config application = SparkUtils.loadApplicationProperty();
-        try (JavaSparkContext sparkContext = SparkUtils.loadSparkContext(application, SPARK_LOCAL_MASTER)) {
-            JobParameter jobParameter = JobParameter.builder().sparkContext(sparkContext).applicationConfig(application).releaseName(RELEASE_NAME).build();
-            proteomeRDD = sparkContext.parallelizePairs(getProteomeTuples());
-            statisticsRDD = sparkContext.parallelizePairs(getStatisticsTuples());
-            ProteomeDocumentsToHPSWriterFake writer = new ProteomeDocumentsToHPSWriterFake(jobParameter);
-            writer.writeIndexDocumentsToHPS();
-            List<ProteomeDocument> savedDocuments = writer.savedDocuments;
-            assertEquals(3, savedDocuments.size());
-            int index = 0;
-            for (ProteomeDocument savedDocument : savedDocuments) {
-                assertResult(savedDocument, index);
-                index++;
-            }
-        }
     }
 
     private void assertResult(ProteomeDocument proteomeDocument, int index) {
@@ -149,28 +151,24 @@ class ProteomeDocumentsToHPSWriterTest {
         Function<ProteomeDocument, ProteomeDocument> getTaxonomyToProteomeDocumentMapper() {
             return input -> {
                 if (Objects.equals(ORGANISM_ID_0, input.organismTaxId)) {
-                    input.organismSort = ORGANISM_SORT_0;
-                    input.organismName = ORGANISM_NAME_0;
-                    input.organismTaxon = ORGANISM_TAXON_0;
-                    input.taxLineageIds = LINEAGE_ID_0;
-                    return input;
+                    return copyTaxData(input, 0);
                 }
                 if (Objects.equals(ORGANISM_ID_1, input.organismTaxId)) {
-                    input.organismSort = ORGANISM_SORT_1;
-                    input.organismName = ORGANISM_NAME_1;
-                    input.organismTaxon = ORGANISM_TAXON_1;
-                    input.taxLineageIds = LINEAGE_ID_1;
-                    return input;
+                    return copyTaxData(input, 1);
                 }
                 if (Objects.equals(ORGANISM_ID_2, input.organismTaxId)) {
-                    input.organismSort = ORGANISM_SORT_2;
-                    input.organismName = ORGANISM_NAME_2;
-                    input.organismTaxon = ORGANISM_TAXON_2;
-                    input.taxLineageIds = LINEAGE_ID_2;
-                    return input;
+                    return copyTaxData(input, 2);
                 }
                 throw new RuntimeException("Invalid input proteome to include tax data");
             };
+        }
+
+        private static ProteomeDocument copyTaxData(ProteomeDocument input, int index) {
+            input.organismSort = ORGANISM_SORTS[index];
+            input.organismName = ORGANISM_NAMES[index];
+            input.organismTaxon = ORGANISM_TAXONS[index];
+            input.taxLineageIds = LINEAGE_IDS[index];
+            return input;
         }
 
         @Override
