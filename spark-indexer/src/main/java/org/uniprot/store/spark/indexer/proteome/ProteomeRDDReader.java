823b1530276fff0a6b34e86b3327b9f1ebe6ad1c
supun <supun@ebi.ac.uk>
2023-08-31 15:25:42 +0100

    xml reader

--- spark-indexer/src/main/java/org/uniprot/store/spark/indexer/proteome/ProteomeRDDReader.java
+++ spark-indexer/src/main/java/org/uniprot/store/spark/indexer/proteome/ProteomeRDDReader.java
@@ -1,7 +1,6 @@
 package org.uniprot.store.spark.indexer.proteome;
 
-import static org.uniprot.store.spark.indexer.common.util.SparkUtils.getInputReleaseDirPath;
-
+import com.typesafe.config.Config;
 import org.apache.spark.api.java.JavaPairRDD;
 import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.sql.Dataset;
@@ -15,13 +14,52 @@ import org.uniprot.store.spark.indexer.common.reader.PairRDDReader;
 import org.uniprot.store.spark.indexer.proteome.converter.DatasetProteomeEntryConverter;
 import org.uniprot.store.spark.indexer.proteome.mapper.ProteomEntryToPair;
 
-import com.typesafe.config.Config;
+import static org.uniprot.store.spark.indexer.common.util.SparkUtils.getInputReleaseDirPath;
 
 /**
  * @author sahmad
  * @created 21/08/2020
  */
 public class ProteomeRDDReader implements PairRDDReader<String, ProteomeEntry> {
+    public static final String UPID = "upid";
+    public static final String TAXONOMY = "taxonomy";
+    public static final String STRAIN = "strain";
+    public static final String MODIFIED = "modified";
+    public static final String IS_REFERENCE_PROTEOME = "isReferenceProteome";
+    public static final String IS_REPRESENTATIVE_PROTEOME = "isRepresentativeProteome";
+    public static final String GENOME_ANNOTATION = "genomeAnnotation";
+    public static final String GENOME_ANNOTATION_SOURCE = "genomeAnnotationSource";
+    public static final String GENOME_ANNOTATION_URL = "genomeAnnotationUrl";
+    public static final String GENOME_ASSEMBLY = "genomeAssembly";
+    public static final String GENOME_ASSEMBLY_SOURCE = "genomeAssemblySource";
+    public static final String GENOME_ASSEMBLY_URL = "genomeAssemblyUrl";
+    public static final String GENOME_REPRESENTATION = "genomeRepresentation";
+    public static final String COMPONENT = "component";
+    public static final String NAME = "_name";
+    public static final String PROTEIN_COUNT = "_proteinCount";
+    public static final String DESCRIPTION = "description";
+    public static final String ANNOTATION_SCORE = "annotationScore";
+    public static final String NORMALIZED_ANNOTATION_SCORE = "_normalizedAnnotationScore";
+    public static final String REFERENCE = "reference";
+    public static final String CITATION = "citation";
+    public static final String TYPE = "_type";
+    public static final String DATE = "_date";
+    public static final String TITLE = "title";
+    public static final String AUTHOR_LIST = "authorList";
+    public static final String PERSON = "person";
+    public static final String DB_REFERENCE = "dbReference";
+    public static final String FIRST = "_first";
+    public static final String LAST = "_last";
+    public static final String VOLUME = "_volume";
+    public static final String DB = "_db";
+    public static final String SCORES = "scores";
+    public static final String VALUE = "_VALUE";
+    public static final String BIO_SAMPLE_ID = "biosampleId";
+    public static final String GENOME_ACCESSION = "genomeAccession";
+    public static final String PROPERTY = "property";
+    public static final String VALUE_LOWER = "value";
+    public static final String CONSORTIUM = "consortium";
+    public static final String ID = "_id";
 
     private final JobParameter jobParameter;
     private final boolean shouldRepartition;
@@ -64,11 +102,126 @@ public class ProteomeRDDReader implements PairRDDReader<String, ProteomeEntry> {
         return data;
     }
 
-    private StructType geProteomeXMLSchema() {
+    public static StructType geProteomeXMLSchema() {
         StructType structType = new StructType();
-        structType = structType.add("upid", DataTypes.StringType, true);
-        structType = structType.add("taxonomy", DataTypes.IntegerType, true);
-        structType = structType.add("isReferenceProteome", DataTypes.BooleanType, true);
+        structType = structType.add(UPID, DataTypes.StringType, true);
+        structType = structType.add(TAXONOMY, DataTypes.LongType, true);
+        structType = structType.add(IS_REFERENCE_PROTEOME, DataTypes.BooleanType, true);
+        structType = structType.add(IS_REPRESENTATIVE_PROTEOME, DataTypes.BooleanType, true);
+        structType = structType.add(MODIFIED, DataTypes.StringType, true);
+        structType = structType.add(STRAIN, DataTypes.StringType, true);
+        structType = structType.add(DESCRIPTION, DataTypes.StringType, true);
+        structType = structType.add(ANNOTATION_SCORE, getAnnotationScoreSchema(), true);
+        structType = structType.add(GENOME_ANNOTATION, getGenomeAnnotationSchema(), true);
+        structType = structType.add(GENOME_ASSEMBLY, getGenomeAssemblySchema(), true);
+        structType = structType.add(COMPONENT, DataTypes.createArrayType(getComponentSchema()), true);
+        structType = structType.add(SCORES, DataTypes.createArrayType(getScoresSchema()), true);
+        structType = structType.add(REFERENCE, DataTypes.createArrayType(getReferenceSchema()), true);
         return structType;
+    }
+
+    public static StructType getReferenceSchema() {
+        StructType reference = new StructType();
+        reference = reference.add(CITATION, getCitationSchema(), true);
+        return reference;
+    }
+
+    public static StructType getCitationSchema() {
+        StructType citation = new StructType();
+        citation = citation.add(DATE, DataTypes.StringType, true);
+        citation = citation.add(DB, DataTypes.StringType, true);
+        citation = citation.add(FIRST, DataTypes.StringType, true);
+        citation = citation.add(LAST, DataTypes.StringType, true);
+        citation = citation.add(NAME, DataTypes.StringType, true);
+        citation = citation.add(TYPE, DataTypes.StringType, true);
+        citation = citation.add(VOLUME, DataTypes.StringType, true);
+        citation = citation.add(TITLE, DataTypes.StringType, true);
+        citation = citation.add(AUTHOR_LIST, getAuthorScheme(), true);
+        citation = citation.add(DB_REFERENCE, DataTypes.createArrayType(getDbReferenceScheme()), true);
+        return citation;
+    }
+
+    public static StructType getDbReferenceScheme() {
+        StructType dbReference = new StructType();
+        dbReference = dbReference.add(VALUE, DataTypes.StringType, true);
+        dbReference = dbReference.add(ID, DataTypes.StringType, true);
+        dbReference = dbReference.add(TYPE, DataTypes.StringType, true);
+        return dbReference;
+    }
+
+    public static StructType getAuthorScheme() {
+        StructType author = new StructType();
+        author = author.add(PERSON, DataTypes.createArrayType(getPersonScheme()), true);
+        author = author.add(CONSORTIUM, getConsortiumSchema(), true);
+        return author;
+    }
+
+    public static StructType getConsortiumSchema() {
+        StructType consortium = new StructType();
+        consortium = consortium.add(VALUE, DataTypes.StringType, true);
+        consortium = consortium.add(NAME, DataTypes.StringType, true);
+        return consortium;
+    }
+
+    public static StructType getPersonScheme() {
+        StructType person = new StructType();
+        person = person.add(VALUE, DataTypes.StringType, true);
+        person = person.add(NAME, DataTypes.StringType, true);
+        return person;
+    }
+
+    public static StructType getScoresSchema() {
+        StructType scores = new StructType();
+        scores = scores.add(NAME, DataTypes.StringType, true);
+        scores = scores.add(PROPERTY, DataTypes.createArrayType((getPropertySchema()), true));
+        return scores;
+    }
+
+    private static StructType getPropertySchema() {
+        StructType property = new StructType();
+        property = property.add(VALUE, DataTypes.StringType, true);
+        property = property.add(NAME, DataTypes.StringType, true);
+        property = property.add(VALUE_LOWER, DataTypes.StringType, true);
+        return property;
+    }
+
+    public static StructType getComponentSchema() {
+        StructType component = new StructType();
+        component = component.add(NAME, DataTypes.StringType, true);
+        component = component.add(PROTEIN_COUNT, DataTypes.LongType, true);
+        component = component.add(BIO_SAMPLE_ID, DataTypes.StringType, true);
+        component = component.add(DESCRIPTION, DataTypes.StringType, true);
+        component = component.add(GENOME_ACCESSION, DataTypes.StringType, true);
+        component = component.add(GENOME_ANNOTATION, getGenomeAnnotationSourceSchema(), true);
+        return component;
+    }
+
+    public static StructType getGenomeAnnotationSourceSchema() {
+        StructType genomeAnnotationSource = new StructType();
+        genomeAnnotationSource = genomeAnnotationSource.add(GENOME_ANNOTATION_SOURCE, DataTypes.StringType, true);
+        return genomeAnnotationSource;
+    }
+
+    public static StructType getGenomeAssemblySchema() {
+        StructType genomeAssembly = new StructType();
+        genomeAssembly = genomeAssembly.add(GENOME_ASSEMBLY, DataTypes.StringType, true);
+        genomeAssembly = genomeAssembly.add(GENOME_ASSEMBLY_URL, DataTypes.StringType, true);
+        genomeAssembly = genomeAssembly.add(GENOME_ASSEMBLY_SOURCE, DataTypes.StringType, true);
+        genomeAssembly = genomeAssembly.add(GENOME_REPRESENTATION, DataTypes.StringType, true);
+        return genomeAssembly;
+    }
+
+    public static StructType getAnnotationScoreSchema() {
+        StructType annotationScore = new StructType();
+        annotationScore = annotationScore.add(VALUE, DataTypes.StringType, true);
+        annotationScore = annotationScore.add(NORMALIZED_ANNOTATION_SCORE, DataTypes.LongType, true);
+        return annotationScore;
+    }
+
+    public static StructType getGenomeAnnotationSchema() {
+        StructType genomeAnnotation = new StructType();
+        genomeAnnotation = genomeAnnotation.add(GENOME_ANNOTATION_SOURCE, DataTypes.StringType, true);
+        genomeAnnotation = genomeAnnotation.add(GENOME_ANNOTATION_URL, DataTypes.StringType, true);
+        return genomeAnnotation;
     }
 }
--- spark-indexer/src/main/java/org/uniprot/store/spark/indexer/proteome/converter/DatasetProteomeEntryConverter.java
+++ spark-indexer/src/main/java/org/uniprot/store/spark/indexer/proteome/converter/DatasetProteomeEntryConverter.java
@@ -1,16 +1,33 @@
 package org.uniprot.store.spark.indexer.proteome.converter;
 
-import static org.uniprot.store.spark.indexer.common.util.RowUtils.*;
-
-import java.io.Serializable;
-
 import org.apache.spark.api.java.function.Function;
 import org.apache.spark.sql.Row;
-import org.uniprot.core.proteome.ProteomeEntry;
-import org.uniprot.core.proteome.ProteomeType;
+import org.uniprot.core.CrossReference;
+import org.uniprot.core.citation.Citation;
+import org.uniprot.core.citation.CitationDatabase;
+import org.uniprot.core.citation.CitationType;
+import org.uniprot.core.citation.SubmissionDatabase;
+import org.uniprot.core.citation.impl.*;
+import org.uniprot.core.impl.CrossReferenceBuilder;
+import org.uniprot.core.proteome.*;
+import org.uniprot.core.proteome.impl.ComponentBuilder;
+import org.uniprot.core.proteome.impl.GenomeAnnotationBuilder;
+import org.uniprot.core.proteome.impl.GenomeAssemblyBuilder;
 import org.uniprot.core.proteome.impl.ProteomeEntryBuilder;
 import org.uniprot.core.uniprotkb.taxonomy.Taxonomy;
 import org.uniprot.core.uniprotkb.taxonomy.impl.TaxonomyBuilder;
+
+import java.io.Serializable;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Collection;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Objects;
+import java.util.stream.Collectors;
+
+import static org.uniprot.store.spark.indexer.common.util.RowUtils.hasFieldName;
+import static org.uniprot.store.spark.indexer.proteome.ProteomeRDDReader.*;
 
 /**
  * Converts XML {@link Row} instances to {@link ProteomeEntry} instances.
@@ -25,20 +42,274 @@ public class DatasetProteomeEntryConverter implements Function<Row, ProteomeEntr
     @Override
     public ProteomeEntry call(Row row) throws Exception {
         ProteomeEntryBuilder builder = new ProteomeEntryBuilder();
-        builder.proteomeId(row.getString(row.fieldIndex("upid")));
-        // put upid and taxonomy from xml
-        Taxonomy taxonomy =
-                new TaxonomyBuilder().taxonId(row.getInt(row.fieldIndex("taxonomy"))).build();
-        builder.taxonomy(taxonomy);
-        // add if is a reference proteome
-        ProteomeType type = ProteomeType.NORMAL;
-        if (hasFieldName("isReferenceProteome", row)) {
-            boolean isReference = row.getBoolean(row.fieldIndex("isReferenceProteome"));
-            if (isReference) {
-                type = ProteomeType.REFERENCE;
+        // upid
+        if (hasFieldName(UPID, row)) {
+            builder.proteomeId(row.getString(row.fieldIndex(UPID)));
+        }
+        //taxonomy
+        if (hasFieldName(TAXONOMY, row)) {
+            Taxonomy taxonomy = new TaxonomyBuilder().taxonId(row.getInt(row.fieldIndex(TAXONOMY))).build();
+            builder.taxonomy(taxonomy);
+        }
+        //strain
+        if (hasFieldName(STRAIN, row)) {
+            builder.strain(row.getString(row.fieldIndex(STRAIN)));
+        }
+        //strain
+        if (hasFieldName(DESCRIPTION, row)) {
+            builder.description(row.getString(row.fieldIndex(DESCRIPTION)));
+        }
+        //modified
+        if (hasFieldName(MODIFIED, row)) {
+            DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+            builder.modified(LocalDate.parse(row.getString(row.fieldIndex(MODIFIED)), formatter));
+        }
+        // proteome type
+        ProteomeType proteomeType = ProteomeType.NORMAL;
+        boolean isReference = false;
+        boolean isRepresentative = false;
+        if (hasFieldName(IS_REFERENCE_PROTEOME, row)) {
+            isReference = row.getBoolean(row.fieldIndex(IS_REFERENCE_PROTEOME));
+        }
+        if (hasFieldName(IS_REPRESENTATIVE_PROTEOME, row)) {
+            isRepresentative = row.getBoolean(row.fieldIndex(IS_REPRESENTATIVE_PROTEOME));
+        }
+        if (isReference && isRepresentative) {
+            proteomeType = ProteomeType.REFERENCE_AND_REPRESENTATIVE;
+        } else if (isReference) {
+            proteomeType = ProteomeType.REFERENCE;
+        } else if (isRepresentative) {
+            proteomeType = ProteomeType.REPRESENTATIVE;
+        }
+        builder.proteomeType(proteomeType);
+        //genome annotation
+        if (hasFieldName(GENOME_ANNOTATION, row)) {
+            GenomeAnnotation genomeAnnotation = getGenomeAnnotation((Row) row.get(row.fieldIndex(GENOME_ANNOTATION)));
+            builder.genomeAnnotation(genomeAnnotation);
+        }
+        //genome assembly
+        if (hasFieldName(GENOME_ASSEMBLY, row)) {
+            GenomeAssembly genomeAssembly = getGenomeAssembly((Row) row.get(row.fieldIndex(GENOME_ASSEMBLY)));
+            builder.genomeAssembly(genomeAssembly);
+        }
+        //annotation score
+        if (hasFieldName(ANNOTATION_SCORE, row)) {
+            Integer annotationScore = getAnnotationScore((Row) row.get(row.fieldIndex(ANNOTATION_SCORE)));
+            builder.annotationScore(annotationScore);
+        }
+        //component set
+        if (hasFieldName(COMPONENT, row)) {
+            List<Row> componentRows = row.getList(row.fieldIndex(COMPONENT));
+            builder.componentsSet(componentRows.stream().map(this::getComponent).collect(Collectors.toList()));
+        }
+        //citation set
+        if (hasFieldName(REFERENCE, row)) {
+            List<Row> referenceRows = row.getList(row.fieldIndex(REFERENCE));
+            builder.citationsSet(referenceRows.stream().map(this::getCitation).filter(Objects::nonNull).collect(Collectors.toList()));
+        }
+
+        return builder.build();
+    }
+
+    private Citation getCitation(Row row) {
+        if (hasFieldName(CITATION, row)) {
+            Row citationRow = (Row) row.get(row.fieldIndex(CITATION));
+            return getCitationItem(citationRow);
+        }
+        return null;
+    }
+
+    private Citation getCitationItem(Row row) {
+        if (hasFieldName(TYPE, row)) {
+            CitationType citationType = CitationType.typeOf(row.getString(row.fieldIndex(TYPE)));
+            switch (citationType) {
+                case BOOK:
+                    return getBook(row);
+                case PATENT:
+                    return getPatent(row);
+                case THESIS:
+                    return getThesis(row);
+                case SUBMISSION:
+                    return getSubmission(row);
+                case JOURNAL_ARTICLE:
+                    return getJournalArticle(row);
+                case ELECTRONIC_ARTICLE:
+                    return getElectronicArticle(row);
+                case LITERATURE:
+                    return getLiterature(row);
+                case UNPUBLISHED:
+                    return getUnpublished(row);
             }
         }
-        builder.proteomeType(type);
-        return builder.build();
+        return null;
+    }
+
+    private Citation getBook(Row row) {
+        BookBuilder bookBuilder = new BookBuilder();
+        populateCommon(row, bookBuilder);
+        if (hasFieldName(FIRST, row)) {
+            bookBuilder.firstPage(String.valueOf(row.getLong(row.fieldIndex(FIRST))));
+        }
+        if (hasFieldName(LAST, row)) {
+            bookBuilder.lastPage(String.valueOf(row.getLong(row.fieldIndex(LAST))));
+        }
+        if (hasFieldName(VOLUME, row)) {
+            bookBuilder.volume(String.valueOf(row.getLong(row.fieldIndex(VOLUME))));
+        }
+        return bookBuilder.build();
+    }
+
+    private Citation getPatent(Row row) {
+        PatentBuilder patentBuilder = new PatentBuilder();
+        populateCommon(row, patentBuilder);
+        return patentBuilder.build();
+    }
+
+    private Citation getThesis(Row row) {
+        ThesisBuilder thesisBuilder = new ThesisBuilder();
+        populateCommon(row, thesisBuilder);
+        return thesisBuilder.build();
+    }
+
+    private Citation getSubmission(Row row) {
+        SubmissionBuilder submissionBuilder = new SubmissionBuilder();
+        populateCommon(row, submissionBuilder);
+        if (hasFieldName(DB, row)) {
+            submissionBuilder.submittedToDatabase(SubmissionDatabase.typeOf(row.getString(row.fieldIndex(DB))));
+        }
+        return submissionBuilder.build();
+    }
+
+    private Citation getJournalArticle(Row row) {
+        JournalArticleBuilder journalArticleBuilder = new JournalArticleBuilder();
+        populateCommon(row, journalArticleBuilder);
+        if (hasFieldName(FIRST, row)) {
+            journalArticleBuilder.firstPage(String.valueOf(row.getLong(row.fieldIndex(FIRST))));
+        }
+        if (hasFieldName(LAST, row)) {
+            journalArticleBuilder.lastPage(String.valueOf(row.getLong(row.fieldIndex(LAST))));
+        }
+        if (hasFieldName(VOLUME, row)) {
+            journalArticleBuilder.volume(String.valueOf(row.getLong(row.fieldIndex(VOLUME))));
+        }
+        if (hasFieldName(NAME, row)) {
+            journalArticleBuilder.journalName(row.getString(row.fieldIndex(NAME)));
+        }
+        return journalArticleBuilder.build();
+    }
+
+    private Citation getElectronicArticle(Row row) {
+        ElectronicArticleBuilder electronicArticleBuilder = new ElectronicArticleBuilder();
+        populateCommon(row, electronicArticleBuilder);
+        return electronicArticleBuilder.build();
+    }
+
+    private Citation getLiterature(Row row) {
+        LiteratureBuilder literatureBuilder = new LiteratureBuilder();
+        populateCommon(row, literatureBuilder);
+        return literatureBuilder.build();
+    }
+
+    private Citation getUnpublished(Row row) {
+        UnpublishedBuilder unpublishedBuilder = new UnpublishedBuilder();
+        populateCommon(row, unpublishedBuilder);
+        return unpublishedBuilder.build();
+    }
+
+    private void populateCommon(Row row, AbstractCitationBuilder citationBuilder) {
+        if (hasFieldName(AUTHOR_LIST, row)) {
+            Row authorsRow = (Row) row.get(row.fieldIndex(AUTHOR_LIST));
+            citationBuilder.authorsSet(getAuthors(authorsRow));
+        }
+        if (hasFieldName(DB_REFERENCE, row)) {
+            List<Row> dbReferences = row.getList(row.fieldIndex(DB_REFERENCE));
+            citationBuilder.citationCrossReferencesSet(dbReferences.stream().map(this::getCrossRef).filter(Objects::nonNull).collect(Collectors.toList()));
+        }
+        if (hasFieldName(TITLE, row)) {
+            citationBuilder.title((row.getString(row.fieldIndex(TITLE))));
+        }
+        if (hasFieldName(DATE, row)) {
+            citationBuilder.publicationDate((row.getString(row.fieldIndex(DATE))));
+        }
+    }
+
+    private CrossReference<CitationDatabase> getCrossRef(Row row) {
+        if (hasFieldName(TYPE, row)) {
+            return new CrossReferenceBuilder<CitationDatabase>()
+                    .database(CitationDatabase.typeOf(row.getString(row.fieldIndex(TYPE))))
+                    .id(row.getString(row.fieldIndex(ID)))
+                    .build();
+        }
+        return null;
+    }
+
+    private Collection<String> getAuthors(Row row) {
+        List<String> authorList = new LinkedList<>();
+        if (hasFieldName(PERSON, row)) {
+            List<Row> personList = row.getList(row.fieldIndex(PERSON));
+            authorList.addAll(personList.stream().map(this::getName).filter(Objects::nonNull).collect(Collectors.toList()));
+        }
+        return authorList;
+    }
+
+    private String getName(Row row) {
+        if (hasFieldName(NAME, row)) {
+            return row.getString(row.fieldIndex(NAME));
+        }
+        return null;
+    }
+
+    private Integer getAnnotationScore(Row row) {
+        Integer annotationScore = null;
+        if (hasFieldName(NORMALIZED_ANNOTATION_SCORE, row)) {
+            annotationScore = (int) row.getLong(row.fieldIndex(NORMALIZED_ANNOTATION_SCORE));
+        }
+        return annotationScore;
+    }
+
+    private Component getComponent(Row row) {
+        ComponentBuilder componentBuilder = new ComponentBuilder();
+        if (hasFieldName(NAME, row)) {
+            componentBuilder.name(row.getString(row.fieldIndex(NAME)));
+        }
+        if (hasFieldName(PROTEIN_COUNT, row)) {
+            componentBuilder.proteinCount((int) row.getLong(row.fieldIndex(PROTEIN_COUNT)));
+        }
+        if (hasFieldName(DESCRIPTION, row)) {
+            componentBuilder.description(row.getString(row.fieldIndex(DESCRIPTION)));
+        }
+        if (hasFieldName(GENOME_ANNOTATION, row)) {
+            GenomeAnnotation genomeAnnotation = getGenomeAnnotation((Row) row.get(row.fieldIndex(GENOME_ANNOTATION)));
+            componentBuilder.genomeAnnotation(genomeAnnotation);
+        }
+        return componentBuilder.build();
+    }
+
+    private GenomeAssembly getGenomeAssembly(Row row) {
+        GenomeAssemblyBuilder genomeAssemblyBuilder = new GenomeAssemblyBuilder();
+        if (hasFieldName(GENOME_ASSEMBLY, row)) {
+            genomeAssemblyBuilder.assemblyId(row.getString(row.fieldIndex(GENOME_ASSEMBLY)));
+        }
+        if (hasFieldName(GENOME_ASSEMBLY_SOURCE, row)) {
+            genomeAssemblyBuilder.source(GenomeAssemblySource.fromValue((row.getString(row.fieldIndex(GENOME_ASSEMBLY_SOURCE)))));
+        }
+        if (hasFieldName(GENOME_ASSEMBLY_URL, row)) {
+            genomeAssemblyBuilder.genomeAssemblyUrl(row.getString(row.fieldIndex(GENOME_ASSEMBLY_URL)));
+        }
+        if (hasFieldName(GENOME_REPRESENTATION, row)) {
+            genomeAssemblyBuilder.level(GenomeAssemblyLevel.fromValue(row.getString(row.fieldIndex(GENOME_REPRESENTATION))));
+        }
+        return genomeAssemblyBuilder.build();
+    }
+
+    private GenomeAnnotation getGenomeAnnotation(Row row) {
+        GenomeAnnotationBuilder genomeAnnotationBuilder = new GenomeAnnotationBuilder();
+        if (hasFieldName(GENOME_ANNOTATION_SOURCE, row)) {
+            genomeAnnotationBuilder.source(row.getString(row.fieldIndex(GENOME_ANNOTATION_SOURCE)));
+        }
+        if (hasFieldName(GENOME_ANNOTATION_URL, row)) {
+            genomeAnnotationBuilder.url(row.getString(row.fieldIndex(GENOME_ANNOTATION_URL)));
+        }
+        return genomeAnnotationBuilder.build();
     }
 }
