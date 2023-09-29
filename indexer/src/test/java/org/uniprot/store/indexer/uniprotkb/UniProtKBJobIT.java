23b6f50d4acc534e6e2e7f036b12d062e35322e2
LeonardoGonzales <lgonzales@ebi.ac.uk>
2023-09-29 16:11:54 +0100

    TRM-29901: new xref pumba

--- indexer/src/test/java/org/uniprot/store/indexer/uniprotkb/UniProtKBJobIT.java
+++ indexer/src/test/java/org/uniprot/store/indexer/uniprotkb/UniProtKBJobIT.java
@@ -131,7 +131,7 @@ class UniProtKBJobIT {
         List<SuggestDocument> response =
                 solrClient.query(
                         SolrCollection.suggest,
-                        new SolrQuery("*:*").setRows(300),
+                        new SolrQuery("*:*").setRows(400),
                         SuggestDocument.class);
         assertThat(response, is(notNullValue()));
         assertThat(response.size(), is(reportedWriteCount));
