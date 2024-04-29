8fa20ae6c9e4f74be72812ed6e316e64f8359921
Shadab Ahmad <sahmad@ebi.ac.uk>
2024-04-29 13:45:12 +0100

    remove kegg

--- indexer/src/test/java/org/uniprot/store/indexer/uniprotkb/converter/UniProtKBEntryConverterIT.java
+++ indexer/src/test/java/org/uniprot/store/indexer/uniprotkb/converter/UniProtKBEntryConverterIT.java
@@ -325,13 +325,13 @@ class UniProtKBEntryConverterIT {
         assertEquals(0, doc.organismHostNames.size());
         assertEquals(0, doc.organismHostIds.size());
 
-        assertEquals(153, doc.crossRefs.size());
+        assertEquals(151, doc.crossRefs.size());
         assertTrue(doc.crossRefs.contains("refseq-NM_001270626.1"));
         assertTrue(doc.crossRefs.contains("refseq-NM_001270626"));
         assertTrue(doc.crossRefs.contains("NM_001270626.1"));
         assertTrue(doc.crossRefs.contains("NM_001270626"));
 
-        assertEquals(21, doc.databases.size());
+        assertEquals(20, doc.databases.size());
         assertTrue(doc.databases.contains("refseq"));
         assertTrue(doc.databases.contains("ensembl"));
 
