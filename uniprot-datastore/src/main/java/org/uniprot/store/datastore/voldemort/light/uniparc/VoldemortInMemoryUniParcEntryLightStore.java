7699c8b7497a22b143ba26d409450161cdb4dc0f
Shadab Ahmad <sahmad@ebi.ac.uk>
2024-06-19 17:05:05 +0100

    initial commit for voldemort changes

---
+++ uniprot-datastore/src/main/java/org/uniprot/store/datastore/voldemort/light/uniparc/VoldemortInMemoryUniParcEntryLightStore.java
@@ -0,0 +1,24 @@
+package org.uniprot.store.datastore.voldemort.light.uniparc;
+
+import org.uniprot.core.uniparc.UniParcEntryLight;
+import org.uniprot.store.datastore.voldemort.VoldemortInMemoryEntryStore;
+
+public class VoldemortInMemoryUniParcEntryLightStore extends VoldemortInMemoryEntryStore<UniParcEntryLight> {
+    private static VoldemortInMemoryUniParcEntryLightStore instance;
+
+    public static VoldemortInMemoryUniParcEntryLightStore getInstance(String storeName) {
+        if (instance == null) {
+            instance = new VoldemortInMemoryUniParcEntryLightStore(storeName);
+        }
+        return instance;
+    }
+
+    private VoldemortInMemoryUniParcEntryLightStore(String storeName) {
+        super(storeName);
+    }
+
+    @Override
+    public String getStoreId(UniParcEntryLight entry) {
+        return entry.getUniParcId().getValue();
+    }
+}
