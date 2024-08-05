d3574beedf6c899d20e18ed48b9b98f7453b5ff2
Shadab Ahmad <sahmad@ebi.ac.uk>
2024-08-05 13:46:07 +0100

    use UniParcCrossReferencePair while saving in voldemort

--- spark-indexer/src/main/java/org/uniprot/store/spark/indexer/uniparc/mapper/UniParcCrossReferenceMapper.java
+++ spark-indexer/src/main/java/org/uniprot/store/spark/indexer/uniparc/mapper/UniParcCrossReferenceMapper.java
@@ -1,18 +1,21 @@
 package org.uniprot.store.spark.indexer.uniparc.mapper;
 
-import java.util.ArrayList;
-import java.util.Iterator;
-import java.util.List;
-
 import org.apache.spark.api.java.function.FlatMapFunction;
 import org.uniprot.core.uniparc.UniParcCrossReference;
 import org.uniprot.core.uniparc.UniParcEntry;
 import org.uniprot.core.util.Pair;
-import org.uniprot.core.util.PairImpl;
+import org.uniprot.store.spark.indexer.uniparc.UniParcCrossReferencePair;
+
+import java.io.Serial;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 
 public class UniParcCrossReferenceMapper
         implements FlatMapFunction<UniParcEntry, Pair<String, List<UniParcCrossReference>>> {
 
+    @Serial
+    private static final long serialVersionUID = -3650716308685435024L;
     private final int batchSize;
 
     public UniParcCrossReferenceMapper(int batchSize){
@@ -30,12 +33,12 @@ public class UniParcCrossReferenceMapper
         for (UniParcCrossReference xref: uniParcEntry.getUniParcCrossReferences()) {
             batchItems.add(xref);
             if(++xrefIndex % batchSize == 0) {
-                result.add(new PairImpl<>(uniParcId + "_" + batchIndex++, batchItems));
+                result.add(new UniParcCrossReferencePair(uniParcId + "_" + batchIndex++, batchItems));
                 batchItems = new ArrayList<>();
             }
         }
         if(!batchItems.isEmpty()){
-            result.add(new PairImpl<>(uniParcId + "_" + batchIndex, batchItems));
+            result.add(new UniParcCrossReferencePair(uniParcId + "_" + batchIndex, batchItems));
         }
         return result.iterator();
     }
