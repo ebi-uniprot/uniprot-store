83267dafd43df350c8cfab41f8cf5b9c6cd575ed
LeonardoGonzales <lgonzales@ebi.ac.uk>
2024-08-05 18:26:35 +0100

    Add mapper test

--- spark-indexer/src/main/java/org/uniprot/store/spark/indexer/uniparc/mapper/UniParcCrossReferenceMapper.java
+++ spark-indexer/src/main/java/org/uniprot/store/spark/indexer/uniparc/mapper/UniParcCrossReferenceMapper.java
@@ -1,15 +1,15 @@
 package org.uniprot.store.spark.indexer.uniparc.mapper;
 
-import org.apache.spark.api.java.function.FlatMapFunction;
-import org.uniprot.core.uniparc.UniParcCrossReference;
-import org.uniprot.core.uniparc.UniParcEntry;
-import org.uniprot.core.util.Pair;
-import org.uniprot.core.uniparc.impl.UniParcCrossReferencePair;
-
 import java.io.Serial;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
+
+import org.apache.spark.api.java.function.FlatMapFunction;
+import org.uniprot.core.uniparc.UniParcCrossReference;
+import org.uniprot.core.uniparc.UniParcEntry;
+import org.uniprot.core.util.Pair;
+import org.uniprot.core.util.PairImpl;
 
 public class UniParcCrossReferenceMapper
         implements FlatMapFunction<UniParcEntry, Pair<String, List<UniParcCrossReference>>> {
@@ -25,20 +25,15 @@ public class UniParcCrossReferenceMapper
     @Override
     public Iterator<Pair<String, List<UniParcCrossReference>>> call(UniParcEntry uniParcEntry)
             throws Exception {
-        int batchIndex = 0;
-        int xrefIndex = 0;
-        String uniParcId = uniParcEntry.getUniParcId().getValue();
-        List<UniParcCrossReference> batchItems = new ArrayList<>();
         List<Pair<String, List<UniParcCrossReference>>> result = new ArrayList<>();
-        for (UniParcCrossReference xref: uniParcEntry.getUniParcCrossReferences()) {
-            batchItems.add(xref);
-            if(++xrefIndex % batchSize == 0) {
-                result.add(new UniParcCrossReferencePair(uniParcId + "_" + batchIndex++, batchItems));
-                batchItems = new ArrayList<>();
-            }
-        }
-        if(!batchItems.isEmpty()){
-            result.add(new UniParcCrossReferencePair(uniParcId + "_" + batchIndex, batchItems));
+        int crossRefSize = uniParcEntry.getUniParcCrossReferences().size();
+        String uniParcId = uniParcEntry.getUniParcId().getValue();
+        int batchIndex = 0;
+        for (int i = 0; i < crossRefSize; i = i + batchSize) {
+            List<UniParcCrossReference> batchItems =
+                    uniParcEntry.getUniParcCrossReferences()
+                            .subList(i, Math.min(i + batchSize, crossRefSize));
+            result.add(new PairImpl<>(uniParcId + "_" + batchIndex++, new ArrayList<>(batchItems)));
         }
         return result.iterator();
     }
--- spark-indexer/src/test/java/org/uniprot/store/spark/indexer/uniparc/mapper/UniParcCrossReferenceMapperTest.java
+++ spark-indexer/src/test/java/org/uniprot/store/spark/indexer/uniparc/mapper/UniParcCrossReferenceMapperTest.java
@@ -1,13 +1,106 @@
 package org.uniprot.store.spark.indexer.uniparc.mapper;
 
 import org.junit.jupiter.api.Test;
+import org.uniprot.core.uniparc.UniParcCrossReference;
+import org.uniprot.core.uniparc.UniParcDatabase;
+import org.uniprot.core.uniparc.UniParcEntry;
+import org.uniprot.core.uniparc.impl.UniParcCrossReferenceBuilder;
+import org.uniprot.core.uniparc.impl.UniParcEntryBuilder;
+import org.uniprot.core.util.Pair;
+
+import java.util.Iterator;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class UniParcCrossReferenceMapperTest {
 
+    private static final String UNIPARC_ID = "UPI123456789";
     @Test
-    void call() {
+    void canMapEmpty() throws Exception {
+        UniParcEntry entry = getEntry(0);
+        UniParcCrossReferenceMapper mapper = new UniParcCrossReferenceMapper(3);
+        Iterator<Pair<String, List<UniParcCrossReference>>> result = mapper.call(entry);
+        assertNotNull(result);
+        assertFalse(result.hasNext());
+    }
 
+    @Test
+    void canMapOnePair() throws Exception {
+        UniParcEntry entry = getEntry(2);
+        UniParcCrossReferenceMapper mapper = new UniParcCrossReferenceMapper(3);
+        Iterator<Pair<String, List<UniParcCrossReference>>> result = mapper.call(entry);
+        assertNotNull(result);
+        Pair<String, List<UniParcCrossReference>> xrefPair = result.next();
+        validatePair(xrefPair, 0, List.of("P10000", "P10001"));
+        assertFalse(result.hasNext());
+    }
+
+    @Test
+    void canMapOnePairExact() throws Exception {
+        UniParcEntry entry = getEntry(3);
+        UniParcCrossReferenceMapper mapper = new UniParcCrossReferenceMapper(3);
+        Iterator<Pair<String, List<UniParcCrossReference>>> result = mapper.call(entry);
+        assertNotNull(result);
+        Pair<String, List<UniParcCrossReference>> xrefPair = result.next();
+        validatePair(xrefPair, 0, List.of("P10000", "P10001", "P10002"));
+        assertFalse(result.hasNext());
+    }
+
+    @Test
+    void canMapMultiplePairsExact() throws Exception {
+        UniParcEntry entry = getEntry(6);
+        UniParcCrossReferenceMapper mapper = new UniParcCrossReferenceMapper(3);
+        Iterator<Pair<String, List<UniParcCrossReference>>> result = mapper.call(entry);
+        assertNotNull(result);
+        Pair<String, List<UniParcCrossReference>> xrefPair = result.next();
+        validatePair(xrefPair, 0, List.of("P10000", "P10001", "P10002"));
+        assertTrue(result.hasNext());
+
+        xrefPair = result.next();
+        validatePair(xrefPair, 1, List.of("P10003", "P10004", "P10005"));
+        assertFalse(result.hasNext());
+    }
+
+    @Test
+    void canMapMultiplePairs() throws Exception {
+        UniParcEntry entry = getEntry(7);
+        UniParcCrossReferenceMapper mapper = new UniParcCrossReferenceMapper(3);
+        Iterator<Pair<String, List<UniParcCrossReference>>> result = mapper.call(entry);
+        assertNotNull(result);
+        Pair<String, List<UniParcCrossReference>> xrefPair = result.next();
+        validatePair(xrefPair, 0, List.of("P10000", "P10001", "P10002"));
+        assertTrue(result.hasNext());
+
+        xrefPair = result.next();
+        validatePair(xrefPair, 1, List.of("P10003", "P10004", "P10005"));
+        assertTrue(result.hasNext());
+
+        xrefPair = result.next();
+        validatePair(xrefPair, 2, List.of("P10006"));
+        assertFalse(result.hasNext());
+    }
+
+    private static void validatePair(Pair<String, List<UniParcCrossReference>> xrefPair, int index,  List<String> expectedIds) {
+        assertEquals(UNIPARC_ID+ "_" + index, xrefPair.getKey());
+        List<String> returnedIds = xrefPair.getValue().stream().map(UniParcCrossReference::getId).toList();
+        assertEquals(expectedIds, returnedIds);
+    }
+
+    private UniParcEntry getEntry(int xrefSize){
+        UniParcEntryBuilder builder = new UniParcEntryBuilder()
+                .uniParcId(UNIPARC_ID);
+        for(int i=0;i<xrefSize;i++){
+            builder.uniParcCrossReferencesAdd(getCrossRef(i));
+        }
+        return builder.build();
+    }
+
+    private UniParcCrossReference getCrossRef(int index){
+        return new UniParcCrossReferenceBuilder()
+                        .active(true)
+                        .id("P1000"+index)
+                        .database(UniParcDatabase.SWISSPROT)
+                        .build();
     }
 }
