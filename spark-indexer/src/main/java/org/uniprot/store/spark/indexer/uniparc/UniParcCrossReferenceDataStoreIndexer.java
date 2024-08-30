375acd91d693823e4c9c2d9691c4e4495957defb
LeonardoGonzales <lgonzales@ebi.ac.uk>
2024-08-30 14:31:49 +0100

    add proteome sourceMap

--- spark-indexer/src/main/java/org/uniprot/store/spark/indexer/uniparc/UniParcCrossReferenceDataStoreIndexer.java
+++ spark-indexer/src/main/java/org/uniprot/store/spark/indexer/uniparc/UniParcCrossReferenceDataStoreIndexer.java
@@ -1,15 +1,25 @@
 package org.uniprot.store.spark.indexer.uniparc;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
+import org.apache.spark.api.java.JavaPairRDD;
 import org.apache.spark.api.java.JavaRDD;
+import org.apache.spark.api.java.function.Function2;
 import org.uniprot.core.uniparc.UniParcEntry;
 import org.uniprot.core.uniparc.impl.UniParcCrossReferencePair;
 import org.uniprot.store.spark.indexer.common.JobParameter;
 import org.uniprot.store.spark.indexer.common.store.DataStoreParameter;
+import org.uniprot.store.spark.indexer.uniparc.mapper.UniParSourceJoin;
 import org.uniprot.store.spark.indexer.uniparc.mapper.UniParcCrossReferenceMapper;
+import org.uniprot.store.spark.indexer.uniparc.mapper.UniParcSequenceSourceJoin;
+import org.uniprot.store.spark.indexer.uniprot.UniProtKBUniParcMappingRDDTupleReader;
 
 import com.typesafe.config.Config;
 
 import lombok.extern.slf4j.Slf4j;
+import scala.Tuple2;
 
 @Slf4j
 public class UniParcCrossReferenceDataStoreIndexer extends BaseUniParcDataStoreIndexer {
@@ -28,9 +38,39 @@ public class UniParcCrossReferenceDataStoreIndexer extends BaseUniParcDataStoreI
         int xrefBatchSize = config.getInt("store.uniparc.cross.reference.batchSize");
         // <xrefIdUniqueKey, List<UniParcCrossReference>>
         JavaRDD<UniParcCrossReferencePair> crossRefIdCrossRef =
-                uniParcRDD.flatMap(new UniParcCrossReferenceMapper(xrefBatchSize));
+                uniParcRDD
+                        .mapToPair(e -> new Tuple2<>(e.getUniParcId().getValue(), e))
+                        .leftOuterJoin(loadSequenceSource())
+                        .mapValues(new UniParcSequenceSourceJoin())
+                        .values()
+                        .flatMap(new UniParcCrossReferenceMapper(xrefBatchSize));
         saveInDataStore(crossRefIdCrossRef);
         log.info("Completed UniParc Cross Reference Data Store index");
+    }
+
+    JavaPairRDD<String, Map<String, Set<String>>> loadSequenceSource() {
+        UniProtKBUniParcMappingRDDTupleReader uniProtKBUniParcMapper =
+                new UniProtKBUniParcMappingRDDTupleReader(parameter);
+        // JavaPairRDD<accession, UniParcId>
+        JavaPairRDD<String, String> uniParcJoinRdd = uniProtKBUniParcMapper.load();
+
+        UniParcSequenceSourceMapperRDDTupleReader sourceMapper =
+                new UniParcSequenceSourceMapperRDDTupleReader(parameter);
+        // JavaPairRDD<accession, Set<sourceIds>>
+        JavaPairRDD<String, Set<String>> sourceRDD = sourceMapper.load();
+
+        return uniParcJoinRdd
+                .leftOuterJoin(sourceRDD)
+                .mapToPair(new UniParSourceJoin())
+                .aggregateByKey(new HashMap<>(), aggregate(), aggregate());
+    }
+
+    private Function2<Map<String, Set<String>>, Map<String, Set<String>>, Map<String, Set<String>>>
+            aggregate() {
+        return (s1, s2) -> {
+            s1.putAll(s2);
+            return s1;
+        };
     }
 
     void saveInDataStore(JavaRDD<UniParcCrossReferencePair> uniParcXrefRDD) {
---
+++ spark-indexer/src/main/java/org/uniprot/store/spark/indexer/uniparc/mapper/UniParSourceJoin.java
@@ -0,0 +1,39 @@
+package org.uniprot.store.spark.indexer.uniparc.mapper;
+
+import java.io.Serial;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+import org.apache.spark.api.java.Optional;
+import org.apache.spark.api.java.function.PairFunction;
+
+import scala.Tuple2;
+
+public class UniParSourceJoin
+        implements PairFunction<
+                Tuple2<String, Tuple2<String, Optional<Set<String>>>>,
+                String,
+                Map<String, Set<String>>> {
+    @Serial private static final long serialVersionUID = -3987507162814044120L;
+
+    @Override
+    public Tuple2<String, Map<String, Set<String>>> call(
+            Tuple2<String, Tuple2<String, Optional<Set<String>>>> tuple) throws Exception {
+        String accession = tuple._1;
+        Tuple2<String, Optional<Set<String>>> uniParcMapTuple = tuple._2;
+
+        String uniParcId = uniParcMapTuple._1;
+        Set<String> sources = getSources(uniParcMapTuple._2);
+
+        return new Tuple2<>(uniParcId, Map.of(accession, sources));
+    }
+
+    private Set<String> getSources(Optional<Set<String>> optSources) {
+        Set<String> sources = new HashSet<>();
+        if (optSources.isPresent()) {
+            sources.addAll(optSources.get());
+        }
+        return sources;
+    }
+}
