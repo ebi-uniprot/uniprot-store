adeb9a58a57cb0df04faa70a0161df490b48438a
LeonardoGonzales <lgonzales@ebi.ac.uk>
2024-08-30 14:42:37 +0100

    add proteome component

--- spark-indexer/src/main/java/org/uniprot/store/spark/indexer/uniparc/mapper/UniParcSequenceSourceJoin.java
+++ spark-indexer/src/main/java/org/uniprot/store/spark/indexer/uniparc/mapper/UniParcSequenceSourceJoin.java
@@ -92,10 +92,7 @@ public class UniParcSequenceSourceJoin
                                                 .map(mappedCrossReference::get)
                                                 .filter(xref -> xref.getProteomeId() != null)
                                                 .map(
-                                                        xref ->
-                                                                xref.getId()
-                                                                        + ":"
-                                                                        + xref.getProteomeId())
+                                                        xref -> xref.getId() + ":" + xref.getProteomeId() + ":" + xref.getComponent())
                                                 .collect(Collectors.toSet())));
     }
 }
