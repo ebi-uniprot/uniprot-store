03688cfe0fccdbdbef60c8ac71947aa046c98011
LeonardoGonzales <lgonzales@ebi.ac.uk>
2024-09-17 14:34:11 +0100

    Add another test converting minimal information.

--- common-job/src/test/java/org/uniprot/store/reader/publications/CommunityMappedReferenceConverterTest.java
+++ common-job/src/test/java/org/uniprot/store/reader/publications/CommunityMappedReferenceConverterTest.java
@@ -1,11 +1,11 @@
 package org.uniprot.store.reader.publications;
 
 import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.contains;
-import static org.hamcrest.Matchers.nullValue;
+import static org.hamcrest.Matchers.*;
 import static org.hamcrest.core.Is.is;
 
 import java.time.LocalDate;
+import java.util.Set;
 
 import org.junit.jupiter.api.Test;
 import org.uniprot.core.publication.CommunityAnnotation;
@@ -26,6 +26,27 @@ class CommunityMappedReferenceConverterTest {
                         "Q1MDE9\tORCID\t19597156\t0000-0002-4251-0362\t[Function][Disease & Variants]Protein/gene_name: BraC3; RL3540. Function: BraC3 is an alternative substrate binding component of the ABC transporter braDEFGC. BraC3 supports the transport of leucine, isoleucine, valine, or alanine, but not glutamate or aspartate. Disease: This is a disease. Comments: Transport of branched amino acids by either BraC3 (with BraDEFG) or AapJQMP is required for symbiosis with peas. Date:2022-08-31");
 
         validateConvertsCorrectly(reference);
+    }
+
+    @Test
+    void convertsMinimalInformation() {
+        CommunityMappedReferenceConverter mapper = new CommunityMappedReferenceConverter();
+        CommunityMappedReference reference =
+                mapper.convert(
+                        "Q93QW2\tORCID\t28103676\t0000-0000-0000-0000\t[Function] Date:2024-07-30");
+        assertThat(reference, notNullValue());
+        assertThat(reference.getCitationId(), is("28103676"));
+        assertThat(reference.getUniProtKBAccession().getValue(), is("Q93QW2"));
+        assertThat(reference.getSourceCategories().size(), is(1));
+        assertThat(reference.getSourceCategories(), is(Set.of("Function")));
+        assertThat(reference.getSource().getId(), is("0000-0000-0000-0000"));
+        assertThat(reference.getSource().getName(), is("ORCID"));
+        CommunityAnnotation communityAnnotation = reference.getCommunityAnnotation();
+        assertThat(communityAnnotation.getSubmissionDate(), is(LocalDate.of(2024, 7, 30)));
+        assertThat(communityAnnotation.getComment(), is(nullValue()));
+        assertThat(communityAnnotation.getDisease(), is(nullValue()));
+        assertThat(communityAnnotation.getFunction(), is(nullValue()));
+        assertThat(communityAnnotation.getProteinOrGene(), is(nullValue()));
     }
 
     @Test
