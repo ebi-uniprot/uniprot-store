8d39fc6b84b34e14e575e4085e13bfc1e7639935
LeonardoGonzales <lgonzales@ebi.ac.uk>
2024-09-17 10:34:19 +0100

    TRM-31758: Add submitted date to community publications

--- common-job/src/test/java/org/uniprot/store/reader/publications/CommunityMappedReferenceConverterTest.java
+++ common-job/src/test/java/org/uniprot/store/reader/publications/CommunityMappedReferenceConverterTest.java
@@ -2,7 +2,10 @@ package org.uniprot.store.reader.publications;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.contains;
+import static org.hamcrest.Matchers.nullValue;
 import static org.hamcrest.core.Is.is;
+
+import java.time.LocalDate;
 
 import org.junit.jupiter.api.Test;
 import org.uniprot.core.publication.CommunityAnnotation;
@@ -20,7 +23,7 @@ class CommunityMappedReferenceConverterTest {
         CommunityMappedReferenceConverter mapper = new CommunityMappedReferenceConverter();
         CommunityMappedReference reference =
                 mapper.convert(
-                        "Q1MDE9\tORCID\t19597156\t0000-0002-4251-0362\t[Function][Disease & Variants]Protein/gene_name: BraC3; RL3540. Function: BraC3 is an alternative substrate binding component of the ABC transporter braDEFGC. BraC3 supports the transport of leucine, isoleucine, valine, or alanine, but not glutamate or aspartate. Disease: This is a disease. Comments: Transport of branched amino acids by either BraC3 (with BraDEFG) or AapJQMP is required for symbiosis with peas.");
+                        "Q1MDE9\tORCID\t19597156\t0000-0002-4251-0362\t[Function][Disease & Variants]Protein/gene_name: BraC3; RL3540. Function: BraC3 is an alternative substrate binding component of the ABC transporter braDEFGC. BraC3 supports the transport of leucine, isoleucine, valine, or alanine, but not glutamate or aspartate. Disease: This is a disease. Comments: Transport of branched amino acids by either BraC3 (with BraDEFG) or AapJQMP is required for symbiosis with peas. Date:2022-08-31");
 
         validateConvertsCorrectly(reference);
     }
@@ -30,7 +33,7 @@ class CommunityMappedReferenceConverterTest {
         CommunityMappedReferenceConverter mapper = new CommunityMappedReferenceConverter();
         CommunityMappedReference reference =
                 mapper.convert(
-                        "Q1MDE9\tORCID\t19597156\t0000-0002-4251-0362\t[Function][Disease & Variants]Protein/gene_name:BraC3; RL3540. Function:BraC3 is an alternative substrate binding component of the ABC transporter braDEFGC. BraC3 supports the transport of leucine, isoleucine, valine, or alanine, but not glutamate or aspartate. Disease:This is a disease. Comments:Transport of branched amino acids by either BraC3 (with BraDEFG) or AapJQMP is required for symbiosis with peas.");
+                        "Q1MDE9\tORCID\t19597156\t0000-0002-4251-0362\t[Function][Disease & Variants]Protein/gene_name:BraC3; RL3540. Function:BraC3 is an alternative substrate binding component of the ABC transporter braDEFGC. BraC3 supports the transport of leucine, isoleucine, valine, or alanine, but not glutamate or aspartate. Disease:This is a disease. Comments:Transport of branched amino acids by either BraC3 (with BraDEFG) or AapJQMP is required for symbiosis with peas. Date:2022-08-31");
 
         validateConvertsCorrectly(reference);
     }
@@ -54,6 +57,7 @@ class CommunityMappedReferenceConverterTest {
                 is(
                         "Transport of branched amino acids by either BraC3 (with BraDEFG) or AapJQMP is required for symbiosis with peas."));
         assertThat(communityAnnotation.getDisease(), is("This is a disease."));
+        assertThat(communityAnnotation.getSubmissionDate(), is(LocalDate.of(2022, 8, 31)));
     }
 
     @Test
@@ -75,6 +79,7 @@ class CommunityMappedReferenceConverterTest {
         assertThat(communityAnnotation.getFunction(), is("BraC3"));
         assertThat(communityAnnotation.getComment(), is("Peas"));
         assertThat(communityAnnotation.getDisease(), is("This is a disease."));
+        assertThat(communityAnnotation.getSubmissionDate(), is(nullValue()));
     }
 
     @Test
@@ -82,7 +87,7 @@ class CommunityMappedReferenceConverterTest {
         CommunityMappedReferenceConverter mapper = new CommunityMappedReferenceConverter();
         CommunityMappedReference reference =
                 mapper.convert(
-                        "COMM03\tORCID\t00000003\t0000-0002-7460-6676\t[Function][Subcellular location]Protein/gene_name: EnvP(b). Function: Fusogenic properties.\n");
+                        "COMM03\tORCID\t00000003\t0000-0002-7460-6676\t[Function][Subcellular location]Protein/gene_name: EnvP(b). Function: Fusogenic properties.Date:2022-09-16\n");
 
         validateMultipleAnnotationsAndMultipleCategories(reference);
     }
@@ -93,7 +98,7 @@ class CommunityMappedReferenceConverterTest {
         CommunityMappedReferenceConverter mapper = new CommunityMappedReferenceConverter();
         CommunityMappedReference reference =
                 mapper.convert(
-                        "COMM03\tORCID\t00000003\t0000-0002-7460-6676\t[Function][Subcellular location]Protein/gene_name:EnvP(b). Function:Fusogenic properties.\n");
+                        "COMM03\tORCID\t00000003\t0000-0002-7460-6676\t[Function][Subcellular location]Protein/gene_name:EnvP(b). Function:Fusogenic properties.Date:2022-09-16\n");
 
         validateMultipleAnnotationsAndMultipleCategories(reference);
     }
@@ -108,5 +113,8 @@ class CommunityMappedReferenceConverterTest {
         assertThat(reference.getSourceCategories(), contains("Function", "Subcellular Location"));
         assertThat(reference.getCommunityAnnotation().getProteinOrGene(), is("EnvP(b)"));
         assertThat(reference.getCommunityAnnotation().getFunction(), is("Fusogenic properties."));
+        assertThat(
+                reference.getCommunityAnnotation().getSubmissionDate(),
+                is(LocalDate.of(2022, 9, 16)));
     }
 }
