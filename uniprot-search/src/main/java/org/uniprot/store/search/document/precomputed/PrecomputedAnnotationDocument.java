34fa058360239bfbf6b608a98ec8c4bba86648c3
Supun Wijerathne <supun@ebi.ac.uk>
2026-05-17 19:29:38 +0100

    make multiple proteome for one precomputedannotation

--- uniprot-search/src/main/java/org/uniprot/store/search/document/precomputed/PrecomputedAnnotationDocument.java
+++ uniprot-search/src/main/java/org/uniprot/store/search/document/precomputed/PrecomputedAnnotationDocument.java
@@ -1,5 +1,8 @@
 package org.uniprot.store.search.document.precomputed;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.apache.solr.client.solrj.beans.Field;
 import org.uniprot.store.search.document.Document;
 
@@ -7,6 +10,7 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Singular;
 
 @Builder(toBuilder = true)
 @Getter
@@ -16,8 +20,9 @@ public class PrecomputedAnnotationDocument implements Document {
     @Field("accession")
     private String accession;
 
+    @Singular("proteome")
     @Field("proteome")
-    private String proteome;
+    private List<String> proteome = new ArrayList<>();
 
     @Override
     public String getDocumentId() {
