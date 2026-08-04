c9d81285db413a1ff95bf48624ade1026a6c8e83
Supun Wijerathne <supun@ebi.ac.uk>
2026-08-04 14:31:13 +0100

    add proteome_canonical to uniprot solr indexing

--- spark-indexer/src/test/java/org/uniprot/store/spark/indexer/uniprot/mapper/UniProtDocumentIsGeneCentricMapperTest.java
+++ spark-indexer/src/test/java/org/uniprot/store/spark/indexer/uniprot/mapper/UniProtDocumentIsGeneCentricMapperTest.java
@@ -1,7 +1,8 @@
 package org.uniprot.store.spark.indexer.uniprot.mapper;
 
-import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.Set;
 
 import org.apache.spark.api.java.Optional;
 import org.junit.jupiter.api.Test;
@@ -15,6 +16,7 @@ class UniProtDocumentIsGeneCentricMapperTest {
     void testCall_WithPresentGeneCentric() throws Exception {
         UniProtDocumentIsGeneCentricMapper mapper = new UniProtDocumentIsGeneCentricMapper();
         UniProtDocument document = new UniProtDocument();
+        document.proteomes = Set.of("P1", "P2");
         boolean expectedGeneCentric = true;
 
         // Tuple with document and a present Optional value.
@@ -26,17 +28,39 @@ class UniProtDocumentIsGeneCentricMapperTest {
 
         // Assert: Check if the result has the expected isGeneCentric value.
         assertTrue(result.isGeneCentric);
+        assertSame(document.proteomes, document.proteomeCanonicals);
+    }
+
+    @Test
+    void testCall_WithNotPresentGeneCentric() throws Exception {
+        UniProtDocumentIsGeneCentricMapper mapper = new UniProtDocumentIsGeneCentricMapper();
+        UniProtDocument document = new UniProtDocument();
+        document.proteomes = Set.of("P1", "P2");
+        boolean expectedGeneCentric = false;
+
+        // Tuple with document and a present Optional value.
+        Tuple2<UniProtDocument, Optional<Boolean>> input =
+                new Tuple2<>(document, Optional.of(expectedGeneCentric));
+
+        // Act: Call the method.
+        UniProtDocument result = mapper.call(input);
+
+        // Assert: Check if the result has the expected isGeneCentric value.
+        assertFalse(result.isGeneCentric);
+        assertEquals(0, result.proteomeCanonicals.size());
     }
 
     @Test
     void testCall_WithEmptyGeneCentric() throws Exception {
         UniProtDocumentIsGeneCentricMapper mapper = new UniProtDocumentIsGeneCentricMapper();
         UniProtDocument document = new UniProtDocument();
+        document.proteomes = Set.of("P1", "P2");
 
         Tuple2<UniProtDocument, Optional<Boolean>> input = new Tuple2<>(document, Optional.empty());
 
         UniProtDocument result = mapper.call(input);
 
         assertFalse(result.isGeneCentric);
+        assertEquals(0, result.proteomeCanonicals.size());
     }
 }
